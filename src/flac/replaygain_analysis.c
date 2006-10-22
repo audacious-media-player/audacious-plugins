e93857fda560302e4ce20273e966651b4dfb6e50
chainsaw <devnull@localhost>
2006-10-21 18:17:49 -0700

    [svn] Flac now plays on 1.13, but not on 1.12. API breakage *sucks*, upstream should stop doing it.

--- src/flac/replaygain_analysis.c
+++ src/flac/replaygain_analysis.c
@@ -14,7 +14,7 @@
  *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  *  concept and filter values by David Robinson (David@Robinson.org)
  *    -- blame him if you think the idea is flawed
@@ -89,12 +89,18 @@
  *  Optimization/clarity suggestions are welcome.
  */
 
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
 #include "replaygain_analysis.h"
+
+Float_t ReplayGainReferenceLoudness = 89.0; /* in dB SPL */
 
 typedef unsigned short  Uint16_t;
 typedef signed short    Int16_t;
