9282822c66d5583bf96b4e3e4aff55059fbac32e
nenolod <devnull@localhost>
2006-12-13 19:45:13 -0800

    [svn] - extension assist for libconsole

--- src/console/Audacious_Driver.cxx
+++ src/console/Audacious_Driver.cxx
@@ -453,6 +453,9 @@ extern "C" void console_aboutbox(void)
 	}
 }
 
+gchar *gme_fmts[] = { "ay", "gbs", "gym", "hes", "kss", "nsf", "nsfe", 
+		      "sap", "spc", "vgm", "vgz", NULL };
+
 InputPlugin console_ip =
 {
 	NULL,
@@ -482,7 +485,8 @@ InputPlugin console_ip =
 	get_song_tuple,
 	NULL,
 	NULL,
-	is_our_file_from_vfs
+	is_our_file_from_vfs,
+	gme_fmts
 };
 
 extern "C" InputPlugin *get_iplugin_info(void)
