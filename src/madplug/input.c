e3a651ade226d577763782d23a77639918c1b88c
js <devnull@localhost>
2007-02-20 06:11:10 -0800

    [svn] Revert r1456, this gives problems with some files with broken ID3-Tags.

--- src/madplug/input.c
+++ src/madplug/input.c
@@ -311,7 +311,7 @@ gchar *input_id3_get_string(struct id3_tag * tag, char *frame_name)
     switch (encoding) {
     case ID3_FIELD_TEXTENCODING_ISO_8859_1:
         rtn0 = (gchar *)id3_ucs4_latin1duplicate(string);
-        rtn = g_convert(rtn0, -1, "utf8", "iso8859-1", NULL, NULL, NULL);
+        rtn = str_to_utf8(rtn0);
         g_free(rtn0);
         break;
     case ID3_FIELD_TEXTENCODING_UTF_8:
