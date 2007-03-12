53c57863c36f368d45f14c345a27bdf67f4f41c1
nenolod <devnull@localhost>
2007-03-12 13:14:40 -0700

    [svn] - make this compile again

--- src/ffmpeg/libavcodec/dsputil.c
+++ src/ffmpeg/libavcodec/dsputil.c
@@ -57,7 +57,7 @@ const uint8_t ff_zigzag248_direct[64] = {
 };
 
 /* not permutated inverse zigzag_direct + 1 for MMX quantizer */
-uint16_t __align8 inv_zigzag_direct16[64];
+uint16_t inv_zigzag_direct16[64];
 
 const uint8_t ff_alternate_horizontal_scan[64] = {
     0,  1,   2,  3,  8,  9, 16, 17, 
