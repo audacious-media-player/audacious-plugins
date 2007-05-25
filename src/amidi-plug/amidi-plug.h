2fe29f92719686bffcf253a86f9e478e9bac5ad8
giacomo <devnull@localhost>
2007-05-25 02:50:20 -0700

    [svn] - amidi-plug: small changes

--- src/amidi-plug/amidi-plug.h
+++ src/amidi-plug/amidi-plug.h
@@ -86,7 +86,7 @@ InputPlugin amidiplug_ip =
 {
   NULL,					/* handle */
   NULL,					/* filename */
-  "AMIDI-Plug" AMIDIPLUG_VERSION " (MIDI Player)", /* description */
+  "AMIDI-Plug " AMIDIPLUG_VERSION " (MIDI Player)", /* description */
   amidiplug_init,			/* init */
   amidiplug_aboutbox,			/* aboutbox */
   amidiplug_configure,			/* configure */
