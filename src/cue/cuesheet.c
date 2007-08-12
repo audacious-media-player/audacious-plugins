4cec0d4fe4a63af7f9f05bae78cbea4b21d17f60
Christian Birchinger <joker@netswarm.net>
2007-08-12 21:53:56 +0200

    Register cue:// with uri_set_plugin

--- src/cue/cuesheet.c
+++ src/cue/cuesheet.c
@@ -141,6 +141,7 @@ static void cue_init(void)
 #ifdef DEBUG
     g_print("watchdog_thread = %p\n", watchdog_thread);
 #endif
+    uri_set_plugin("cue://", &cue_ip);
 }
 
 static void cue_cleanup(void)
