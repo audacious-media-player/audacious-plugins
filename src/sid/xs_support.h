56162cb6bd48b5eade5924c09d01458bb165a0c8
Matti Hamalainen <ccr@tnsp.org>
2007-09-03 01:23:36 +0300

    Move NLS support macros to xs_support.h

--- src/sid/xs_support.h
+++ src/sid/xs_support.h
@@ -43,12 +43,28 @@ extern "C" {
 #endif
 
 
-/* Metadata structures */
-#ifdef AUDACIOUS_PLUGIN
-#define t_xs_tuple Tuple
+/* Standard gettext macros
+ */
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  undef _
+#  define _(String) dgettext (PACKAGE, String)
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop (String)
+#  else
+#    define N_(String) (String)
+#  endif
 #else
-#define t_xs_tuple TitleInput
+#  define _LIBINTL_H
+#  define textdomain(String) (String)
+#  define gettext(String) (String)
+#  define dgettext(Domain,Message) (Message)
+#  define dcgettext(Domain,Message,Type) (Message)
+#  define bindtextdomain(Domain,Directory) (Domain)
+#  define _(String) (String)
+#  define N_(String) (String)
 #endif
+
 
 /* VFS replacement functions
  */
