21f792653bfa7ef1d38f8e1bc6d8c63e0b5d4d56
Matti Hamalainen <ccr@tnsp.org>
2007-09-09 23:22:52 +0300

    Fix some tuple function calls to conform with new API.

--- src/wav/wav.c
+++ src/wav/wav.c
@@ -193,14 +193,14 @@ get_title(const gchar * filename)
 
     tuple = tuple_new_from_filename(filename);
 
-    tuple_associate_string(tuple, "codec", "RIFF/WAV Audio (ADPCM)");
-    tuple_associate_string(tuple, "quality", "lossless");
+    tuple_associate_string(tuple, FIELD_CODEC, NULL, "RIFF/WAV Audio (ADPCM)");
+    tuple_associate_string(tuple, FIELD_QUALITY, NULL, "lossless");
 
     title = tuple_formatter_make_title_string(tuple, get_gentitle_format());
     if (*title == '\0')
     {
         g_free(title);
-        title = g_strdup(tuple_get_string(tuple, "file-name"));
+        title = g_strdup(tuple_get_string(tuple, FIELD_FILE_NAME, NULL));
     }
 
     tuple_free(tuple);
