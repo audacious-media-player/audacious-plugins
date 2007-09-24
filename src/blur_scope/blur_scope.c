34e2e69d2f10431266625acddb0270885bf8e062
William Pitcock <nenolod@atheme.org>
2007-09-24 13:48:53 -0500

    blur_scope: MK2

--- src/blur_scope/blur_scope.c
+++ src/blur_scope/blur_scope.c
@@ -15,14 +15,11 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  w
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#ifdef HAVE_CONFIG_H
-#  include "config.h"
-#endif
+#include "config.h"
 
 #include <glib.h>
 #include <audacious/i18n.h>
