513db034557a5d5086f73eda4e1814e2079cdc91
William Pitcock <nenolod@atheme.org>
2007-10-15 06:20:13 -0500

    paranormal-ng: a GL visualiser. lots to do, most stuff won't work, but hey, this will do cool stuff too soon

---
+++ src/paranormal-ng/containers.h
@@ -0,0 +1,27 @@
+/*
+ * paranormal: iterated pipeline-driven visualization plugin
+ * Copyright (c) 2006, 2007 William Pitcock <nenolod@dereferenced.org>
+ * Portions copyright (c) 2001 Jamie Gennis <jgennis@mindspring.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; under version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _PN_CONTAINERS_H
+#define _PN_CONTAINERS_H
+
+void container_add_actuator (struct pn_actuator *container, struct pn_actuator *a);
+void container_remove_actuator (struct pn_actuator *container, struct pn_actuator *a);
+void container_unlink_actuators (struct pn_actuator *container);
+
+#endif /* _CONTAINERS_H */
