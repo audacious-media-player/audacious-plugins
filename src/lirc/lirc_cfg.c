006ffa3552a7c781d7ae25bdf522c6f3c1874e21
William Pitcock <nenolod@atheme.org>
2008-01-01 16:13:51 -0600

    Add missing files.

---
+++ src/lirc/lirc_cfg.c
@@ -0,0 +1,44 @@
+#include <config.h>
+
+#include <gtk/gtk.h>
+#include <audacious/i18n.h>
+#include <audacious/plugin.h>
+#include <audacious/configdb.h>
+
+#include "common.h"
+
+GtkWidget *lirc_cfg = NULL;
+
+gint b_enable_reconnect;
+gint reconnect_timeout;
+
+void load_cfg(void)
+{
+  ConfigDb *db;
+  db = aud_cfg_db_open();
+  aud_cfg_db_get_int(db, "lirc", "enable_reconnect", &b_enable_reconnect);
+  aud_cfg_db_get_int(db, "lirc", "reconnect_timeout", &reconnect_timeout);
+  if (!reconnect_timeout) reconnect_timeout = 5;
+  aud_cfg_db_close(db);
+}
+
+void save_cfg(void)
+{
+  ConfigDb *db;
+  db = aud_cfg_db_open();
+  aud_cfg_db_set_int(db, "lirc", "enable_reconnect", b_enable_reconnect);
+  aud_cfg_db_set_int(db, "lirc", "reconnect_timeout", reconnect_timeout);
+  aud_cfg_db_close(db);
+}
+
+void configure(void)
+{
+  if (lirc_cfg)
+  {
+    gtk_window_present(GTK_WINDOW(lirc_cfg));
+    return;
+  }
+  load_cfg();
+  lirc_cfg = create_lirc_cfg();
+  gtk_widget_show_all(lirc_cfg);
+}
