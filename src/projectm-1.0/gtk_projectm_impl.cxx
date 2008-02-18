3b7877b936e317e4ab68bd4f78cd70b4d3362f2a
William Pitcock <nenolod@atheme.org>
2008-02-18 01:46:46 -0600

    Reimplement using a GTK+ widget.

--- src/projectm-1.0/gtk_projectm_impl.cxx
+++ src/projectm-1.0/gtk_projectm_impl.cxx
@@ -1,5 +1,5 @@
 /*
- * main.cxx: plugin glue to libprojectm
+ * gtk_projectm_impl.cxx: GTK+ ProjectM Implementation.
  * Copyright (c) 2008 William Pitcock <nenolod@sacredspiral.co.uk>
  * Portions copyright (c) 2004-2006 Peter Sperl
  *
@@ -16,16 +16,9 @@
 #include <gtk/gtk.h>
 #include <gtk/gtkgl.h>
 
-extern "C"
-{
-#include <audacious/util.h>
-#include <audacious/plugin.h>
-#include <audacious/playlist.h>
-#include <audacious/auddrct.h>
-}
+#include "gtk_projectm_impl.h"
 
 #include <math.h>
-#include "ConfigFile.h"
 
 #include <libprojectM/projectM.hpp>
 
@@ -33,157 +26,101 @@ extern "C"
 #define CONFIG_FILE "/share/projectM/config.inp"
 
 // Forward declarations 
-extern "C" void projectM_xmms_init(void);
-extern "C" void projectM_cleanup(void);
-extern "C" void projectM_render_pcm(gint16 pcm_data[2][512]);
-extern "C" int worker_func(void *);
-std::string read_config();
-
-extern "C" VisPlugin projectM_vtable;
+static std::string read_config();
 
 int SDLThreadWrapper(void *);
 void handle_playback_trigger(void *, void *);
 
-static void
-projectM_draw_init(GtkWidget *widget,
-     void *data);
+static void _gtk_projectm_realize_impl(GtkWidget *widget, gpointer data);
+static gboolean _gtk_projectm_redraw_impl(GtkWidget *widget);
+static gboolean _gtk_projectm_expose_impl(GtkWidget *widget, GdkEventExpose *event, gpointer data);
+static gboolean _gtk_projectm_configure_impl(GtkWidget *widget, GdkEventConfigure *event, gpointer data);
 
-static gboolean
-projectM_idle_func(GtkWidget *widget);
-
-static gboolean
-projectM_draw_impl(GtkWidget      *widget,
-      GdkEventExpose *event,
-      gpointer        data);
-
-static gboolean
-projectM_reconfigure(GtkWidget         *widget,
-         GdkEventConfigure *event,
-         gpointer           data);
-
-class projectMPlugin
-{
-  public:
+struct _GtkProjectMPrivate {
     projectM *pm;
-
     GdkGLConfig *glconfig;
-    GtkWidget *window;
-    GtkWidget *vbox;
     GtkWidget *drawing_area;
-    gboolean is_sync;
-    gboolean error;
     gint idle_id;
     GTimer *timer;
     gint frames;
-
-    projectMPlugin()
-    {
-        gtk_gl_init(NULL, NULL);
-
-        this->pm = NULL;
-
-        this->glconfig = gdk_gl_config_new_by_mode((GdkGLConfigMode) (GDK_GL_MODE_RGBA | GDK_GL_MODE_DEPTH | GDK_GL_MODE_DOUBLE));
-        if (!this->glconfig)
-        {
-            this->error++;
-            return;
-        }
-
-        this->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-        gtk_window_set_title(GTK_WINDOW(this->window), "ProjectM");
-        gtk_container_set_reallocate_redraws(GTK_CONTAINER(this->window), TRUE);
-
-        this->vbox = gtk_vbox_new(FALSE, 0);
-        gtk_container_add(GTK_CONTAINER(this->window), this->vbox);
-        gtk_widget_show(this->vbox);
-
-        this->drawing_area = gtk_drawing_area_new();
-        gtk_widget_set_size_request(this->drawing_area, 512, 512);
-        gtk_widget_set_gl_capability(this->drawing_area, this->glconfig, NULL, TRUE, GDK_GL_RGBA_TYPE);
-        gtk_widget_add_events(this->drawing_area, GDK_VISIBILITY_NOTIFY_MASK);
-        gtk_box_pack_start(GTK_BOX(this->vbox), this->drawing_area, TRUE, TRUE, 0);
-
-        g_signal_connect_after(G_OBJECT(this->drawing_area), "realize",
-                         G_CALLBACK(projectM_draw_init), NULL);
-        g_signal_connect(G_OBJECT(this->drawing_area), "expose_event",
-                         G_CALLBACK(projectM_draw_impl), NULL);
-
-        this->timer = g_timer_new();
-        this->frames = 0;
-    }
-
-    ~projectMPlugin()
-    {
-        delete this->pm;
-        this->pm = 0;
-
-        aud_hook_dissociate("playback begin", handle_playback_trigger);
-    }
-
-    void initUI(void)
-    {
-        gtk_widget_show(this->drawing_area);
-        gtk_widget_show(this->window);
-
-        this->idle_id = g_timeout_add (1000 / 30,
-                                       (GSourceFunc) projectM_idle_func,
-                                       this->drawing_area);
-
-        /* XXX */
-        aud_hook_associate("playback begin", handle_playback_trigger, NULL);
-    }
-
-    void addPCMData(gint16 pcm_data[2][512])
-    {
-        if (this->pm)
-            this->pm->pcm->addPCM16(pcm_data);
-    }
-
-    void triggerPlaybackBegin(PlaylistEntry *entry)
-    {
-        std::string title(entry->title);
-
-        if (this->pm)
-            this->pm->projectM_setTitle(title);
-    }
 };
 
-/* glue to implementation section */
-projectMPlugin *thePlugin = 0;
+extern "C" GtkWidget *
+gtk_projectm_new(void)
+{
+    struct _GtkProjectMPrivate *priv = g_slice_new0(struct _GtkProjectMPrivate);
+
+    gtk_gl_init(NULL, NULL);
+
+    priv->glconfig = gdk_gl_config_new_by_mode((GdkGLConfigMode) (GDK_GL_MODE_RGBA | GDK_GL_MODE_DEPTH | GDK_GL_MODE_DOUBLE));
+    if (!priv->glconfig)
+        return NULL;
+
+    priv->drawing_area = gtk_drawing_area_new();
+    gtk_widget_set_size_request(priv->drawing_area, 512, 512);
+    gtk_widget_set_gl_capability(priv->drawing_area, priv->glconfig, NULL, TRUE, GDK_GL_RGBA_TYPE);
+    gtk_widget_add_events(priv->drawing_area, GDK_VISIBILITY_NOTIFY_MASK);
+
+    g_signal_connect_after(G_OBJECT(priv->drawing_area), "realize",
+                           G_CALLBACK(_gtk_projectm_realize_impl), priv);
+    g_signal_connect(G_OBJECT(priv->drawing_area), "expose_event",
+                     G_CALLBACK(_gtk_projectm_expose_impl), priv);
+
+    priv->timer = g_timer_new();
+    priv->frames = 0;
+
+    g_object_set_data(G_OBJECT(priv->drawing_area), "GtkProjectMPrivate", priv);
+
+    return priv->drawing_area;
+}
+
+extern "C" void
+gtk_projectm_add_pcm_data(GtkWidget *widget, gint16 pcm_data[2][512])
+{
+    struct _GtkProjectMPrivate *priv = (struct _GtkProjectMPrivate *) g_object_get_data(G_OBJECT(widget), "GtkProjectMPrivate");
+
+    g_return_if_fail(priv != NULL);
+    g_return_if_fail(priv->pm != NULL);
+
+    priv->pm->pcm->addPCM16(pcm_data);
+}
 
 static void
-projectM_draw_init(GtkWidget *widget,
-     void *data)
+_gtk_projectm_realize_impl(GtkWidget *widget, gpointer data)
 {
     GdkGLContext *glcontext = gtk_widget_get_gl_context(widget);
     GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable(widget);
+    struct _GtkProjectMPrivate *priv = (struct _GtkProjectMPrivate *) data;
 
     if (!gdk_gl_drawable_gl_begin(gldrawable, glcontext))
         return;
 
     std::string configFile = read_config();
-    thePlugin->pm = new projectM(configFile);
-    thePlugin->pm->projectM_resetGL(widget->allocation.width, widget->allocation.height);
+    priv->pm = new projectM(configFile);
+    priv->pm->projectM_resetGL(widget->allocation.width, widget->allocation.height);
 
     gdk_gl_drawable_swap_buffers(gldrawable);
     gdk_gl_drawable_gl_end(gldrawable);
 
     g_signal_connect(G_OBJECT(widget), "configure_event",
-                     G_CALLBACK(projectM_reconfigure), NULL);
+                     G_CALLBACK(_gtk_projectm_configure_impl), NULL);
+
+    priv->idle_id = g_timeout_add (1000 / 30,
+                                   (GSourceFunc) _gtk_projectm_redraw_impl,
+                                   priv->drawing_area);
 }
 
 static gboolean
-projectM_reconfigure(GtkWidget         *widget,
-      GdkEventConfigure *event,
-      gpointer           data)
+_gtk_projectm_configure_impl(GtkWidget *widget, GdkEventConfigure *event, gpointer data)
 {
     GdkGLContext *glcontext = gtk_widget_get_gl_context(widget);
     GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable(widget);
+    struct _GtkProjectMPrivate *priv = (struct _GtkProjectMPrivate *) data;
 
     if (!gdk_gl_drawable_gl_begin(gldrawable, glcontext))
         return FALSE;
 
-    thePlugin->pm->projectM_resetGL(widget->allocation.width, widget->allocation.height);
+    priv->pm->projectM_resetGL(widget->allocation.width, widget->allocation.height);
 
     gdk_gl_drawable_swap_buffers(gldrawable);
     gdk_gl_drawable_gl_end(gldrawable);
@@ -192,81 +129,45 @@ projectM_reconfigure(GtkWidget         *widget,
 }
 
 static gboolean
-projectM_draw_impl(GtkWidget      *widget,
-      GdkEventExpose *event,
-      gpointer        data)
+_gtk_projectm_expose_impl(GtkWidget *widget, GdkEventExpose *event, gpointer data)
 {
     GdkGLContext *glcontext = gtk_widget_get_gl_context(widget);
     GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable(widget);
+    struct _GtkProjectMPrivate *priv = (struct _GtkProjectMPrivate *) data;
 
     if (!gdk_gl_drawable_gl_begin(gldrawable, glcontext))
         return FALSE;
 
-    thePlugin->pm->renderFrame();
-    thePlugin->frames++;
+    priv->pm->renderFrame();
+    priv->frames++;
 
     gdk_gl_drawable_swap_buffers(gldrawable);
     gdk_gl_drawable_gl_end(gldrawable);
 
-    gdouble seconds = g_timer_elapsed (thePlugin->timer, NULL);
+    gdouble seconds = g_timer_elapsed (priv->timer, NULL);
     if (seconds >= 5.0)
     {
-        gdouble fps = thePlugin->frames / seconds;
-        g_print ("%d frames in %6.3f seconds = %6.3f FPS\n", thePlugin->frames, seconds, fps);
-        g_timer_reset (thePlugin->timer);
-        thePlugin->frames = 0;
+        gdouble fps = priv->frames / seconds;
+        g_print ("%d frames in %6.3f seconds = %6.3f FPS\n", priv->frames, seconds, fps);
+        g_timer_reset (priv->timer);
+        priv->frames = 0;
     }
 
     return TRUE;
 }
 
 static gboolean
-projectM_idle_func(GtkWidget *widget)
+_gtk_projectm_redraw_impl(GtkWidget *widget)
 {
     gdk_window_invalidate_rect(widget->window, &widget->allocation, FALSE);
 
     return TRUE;
 }
 
-void handle_playback_trigger(gpointer plentry_p, gpointer unused)
-{
-    PlaylistEntry *entry = reinterpret_cast<PlaylistEntry *>(plentry_p);
-
-    if (!thePlugin)
-        return;
-
-    thePlugin->triggerPlaybackBegin(entry);
-}
-
-extern "C" void projectM_xmms_init(void)
-{
-    if (thePlugin)
-        return;
-
-    thePlugin = new projectMPlugin;
-    thePlugin->initUI();
-}
-
-extern "C" void projectM_cleanup(void)
-{
-    if (!thePlugin)
-        return;
-
-    delete thePlugin;
-}
-
-extern "C" void projectM_render_pcm(gint16 pcm_data[2][512])
-{
-    if (!thePlugin)
-        return;
-
-    thePlugin->addPCMData(pcm_data);
-}
-
 /********************************************************************************
  * XXX: This code is from projectM and still needs to be rewritten!             *
  ********************************************************************************/
-std::string read_config()
+static std::string read_config()
 {
 
 //   int n;
---
+++ src/projectm-1.0/main.c
@@ -0,0 +1,65 @@
+/*
+ * main.cxx: plugin glue to libprojectm
+ * Copyright (c) 2008 William Pitcock <nenolod@sacredspiral.co.uk>
+ * Portions copyright (c) 2004-2006 Peter Sperl
+ *
+ * This program is free software; you may distribute it under the terms
+ * of the GNU General Public License; version 2.
+ */
+
+#include <audacious/plugin.h>
+
+#include "gtk_projectm_impl.h"
+
+GtkWidget *projectm = NULL;
+GtkWidget *window = NULL;
+
+void
+projectM_init(void)
+{
+    GtkWidget *vbox;
+
+    if (window)
+        return;
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "ProjectM");
+    gtk_container_set_reallocate_redraws(GTK_CONTAINER(window), TRUE);
+
+    vbox = gtk_vbox_new(FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+    gtk_widget_show(vbox);
+
+    projectm = gtk_projectm_new();
+    gtk_box_pack_start(GTK_BOX(vbox), projectm, TRUE, TRUE, 0);
+    gtk_widget_show(projectm);
+
+    gtk_widget_show(window);
+}
+
+void
+projectM_cleanup(void)
+{
+
+}
+
+void
+projectM_render_pcm(gint16 pcm_data[2][512])
+{
+    g_return_if_fail(projectm != NULL);
+
+    gtk_projectm_add_pcm_data(projectm, pcm_data);
+}
+
+VisPlugin projectM_vtable = {
+    .description = "projectM",
+    .num_pcm_chs_wanted = 2,
+    .init = projectM_init,
+    .cleanup = projectM_cleanup,
+    .render_pcm = projectM_render_pcm,
+};
+
+VisPlugin *projectM_vplist[] = { &projectM_vtable, NULL };
+
+DECLARE_PLUGIN(projectm, NULL, NULL, NULL, NULL, NULL, NULL,
+        projectM_vplist, NULL);
