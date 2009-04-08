51857493011c670e2bcf31df019e30ffbd71773d
Tony Vroon <chainsaw@gentoo.org>
2009-04-08 23:09:15 +0100

    Remove stray forward slash. Comment-in-comment is bad, hmmkay?

--- src/aac/libfaad2/specrec.c
+++ src/aac/libfaad2/specrec.c
@@ -426,7 +426,7 @@ uint8_t window_grouping_info(NeAACDecStruct *hDecoder, ic_stream *ics)
 }
 
 /* iquant() *
-/* output = sign(input)*abs(input)^(4/3) */
+ * output = sign(input)*abs(input)^(4/3) */
 /**/
 static INLINE real_t iquant(int16_t q, const real_t *tab, uint8_t *error)
 {
