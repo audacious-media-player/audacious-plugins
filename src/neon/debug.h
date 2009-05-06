5cfab516b5b63b571746dbd731c723c6cdf6af65
William Pitcock <nenolod@atheme.org>
2009-05-06 14:06:28 -0500

    define NEON_DEBUG for the moment

--- src/neon/debug.h
+++ src/neon/debug.h
@@ -20,6 +20,8 @@
 
 #include <stdio.h>
 
+#define NEON_DEBUG
+
 #define _ENTER _DEBUG("enter")
 #define _LEAVE _DEBUG("leave"); return
 #define _MESSAGE(tag, string, ...) do { fprintf(stderr, "%s: neon: %s:%d (%s): " string "\n", \
