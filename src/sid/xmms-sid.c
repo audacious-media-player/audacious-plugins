a6b2ec9f566b43de329c5b0d3145ca412ca99975
Matti Hamalainen <ccr@tnsp.org>
2009-06-12 18:58:43 +0300

    Downgrade G_LOG_LEVEL_ERROR to warning, so that we don't uncleanly quit the application due to plugin errors.

--- src/sid/xmms-sid.c
+++ src/sid/xmms-sid.c
@@ -94,7 +94,7 @@ void xs_error(const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    g_logv("AUD-SID", G_LOG_LEVEL_ERROR, fmt, ap);
+    g_logv("AUD-SID", G_LOG_LEVEL_WARNING, fmt, ap);
     va_end(ap);
 }
 
