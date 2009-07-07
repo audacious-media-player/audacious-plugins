f9affaeed9b5902691b3242ceb699e7132493ce0
Tomasz Mon <desowin@gmail.com>
2009-07-07 17:43:42 +0200

    Use designated initializers in modplug plugin

--- src/modplug/plugin.cxx
+++ src/modplug/plugin.cxx
@@ -9,17 +9,17 @@
 #include "gui/main.h"
 extern "C" {
 #include <audacious/plugin.h>
-}
+
 
 extern InputPlugin gModPlug;
 
-static void Init(void)
+void Init(void)
 {
     gModplugXMMS.SetInputPlugin(gModPlug);
     gModplugXMMS.Init();
 }
 
-static int CanPlayFileFromVFS(char* aFilename, VFSFile *VFSFile)
+int CanPlayFileFromVFS(char* aFilename, VFSFile *VFSFile)
 {
     AUDDBG("aFilename=%s\n", aFilename);
     if(gModplugXMMS.CanPlayFileFromVFS(aFilename, VFSFile))
@@ -27,29 +27,29 @@ static int CanPlayFileFromVFS(char* aFilename, VFSFile *VFSFile)
     return 0;
 }
 
-static void PlayFile(InputPlayback *data)
+void PlayFile(InputPlayback *data)
 {
     char* aFilename = data->filename;
     gModplugXMMS.SetOutputPlugin(*data->output);
     gModplugXMMS.PlayFile(aFilename, data);
 }
 
-static void Stop(InputPlayback *data)
+void Stop(InputPlayback *data)
 {
     gModplugXMMS.Stop();
 }
 
-static void Pause(InputPlayback *data, short aPaused)
+void Pause(InputPlayback *data, short aPaused)
 {
     gModplugXMMS.Pause((bool)aPaused);
 }
 
-static void Seek(InputPlayback *data, int aTime)
+void Seek(InputPlayback *data, int aTime)
 {
     gModplugXMMS.Seek(float32(aTime));
 }
 
-static int GetTime(InputPlayback *data)
+int GetTime(InputPlayback *data)
 {
     float32 lTime;
     
@@ -60,7 +60,7 @@ static int GetTime(InputPlayback *data)
         return (int)(lTime * 1000);
 }
 
-static Tuple* GetSongTuple(char* aFilename)
+Tuple* GetSongTuple(char* aFilename)
 {
     return gModplugXMMS.GetSongTuple(aFilename);
 }
@@ -80,50 +80,4 @@ void ShowFileInfoBox(char* aFilename)
     ShowInfoWindow(aFilename);
 }
 
-const gchar *fmts[] =
-    { "amf", "ams", "dbm", "dbf", "dsm", "far", "mdl", "stm", "ult", "mt2",
-      "mdz", "mdr", "mdgz", "mdbz", "mod", "s3z", "s3r", "s3gz", "s3m", "xmz", "xmr", "xmgz",
-      "itz", "itr", "itgz", "dmf", "umx", "it", "669", "xm", "mtm", "psm", "ft2",
-      "zip", "gz", "bz2", "rar", "rb",
-      NULL };
-
-InputPlugin gModPlug =
-{
-    /* Common plugin fields */
-    NULL,
-    NULL,
-    (gchar *)"ModPlug Audio Plugin",
-    Init,
-    NULL,
-    ShowAboutBox,
-    ShowConfigureBox,
-    FALSE,
-
-    /* Input plugin fields */
-    NULL,
-    NULL,
-    PlayFile,
-    Stop,
-    Pause,
-    Seek,
-    GetTime,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    ShowFileInfoBox,
-    GetSongTuple,
-    CanPlayFileFromVFS, // vfs
-    (gchar **)fmts,
-    NULL,
-    NULL,
-    TRUE,   // subtune. to exclude .zip etc which doesn't contain any mod file --yaz
-    NULL
-};
-
-InputPlugin *modplug_iplist[] = { &gModPlug, NULL };
-
-SIMPLE_INPUT_PLUGIN(modplug, modplug_iplist);
+}
