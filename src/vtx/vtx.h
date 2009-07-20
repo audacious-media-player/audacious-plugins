d51575be114cc0f0df3ca2a2dabcb68a4ffffe79
Matti Hamalainen <ccr@tnsp.org>
2009-07-20 18:12:17 +0300

    Remove useless function prototypes, as they are not used outside of vtx.c.

--- src/vtx/vtx.h
+++ src/vtx/vtx.h
@@ -9,16 +9,7 @@
 
 #include <audacious/plugin.h>
 
-void vtx_init(void);
 void vtx_about(void);
 void vtx_file_info(char *filename);
-
-extern int  vtx_is_our_file(char *filename);
-extern void vtx_play_file (InputPlayback *playback);
-extern void vtx_stop (InputPlayback *playback);
-extern void vtx_seek (InputPlayback *playback, int time);
-extern void vtx_pause (InputPlayback *playback, short p);
-extern int  vtx_get_time (InputPlayback *playback);
-extern void vtx_get_song_info (char *filename, char **title, int *length);
 
 #endif
