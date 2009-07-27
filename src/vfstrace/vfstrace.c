b748ce84dbb11fb67db274c10184b315d6bf67d4
Matti Hamalainen <ccr@tnsp.org>
2009-07-27 12:31:55 +0300

    s/size_t/gsize/

--- src/vfstrace/vfstrace.c
+++ src/vfstrace/vfstrace.c
@@ -20,8 +20,8 @@
 
 VFSFile *(*vfs_fopen_impl)(const gchar *path, const gchar *mode) = NULL;
 gint (*vfs_fclose_impl)(VFSFile * file) = NULL;
-size_t (*vfs_fread_impl)(gpointer ptr, size_t size, size_t nmemb, VFSFile *file) = NULL;
-size_t (*vfs_fwrite_impl)(gconstpointer ptr, size_t size, size_t nmemb, VFSFile *file) = NULL;
+gsize (*vfs_fread_impl)(gpointer ptr, gsize size, gsize nmemb, VFSFile *file) = NULL;
+gsize (*vfs_fwrite_impl)(gconstpointer ptr, gsize size, gsize nmemb, VFSFile *file) = NULL;
 gint (*vfs_getc_impl)(VFSFile *stream) = NULL;
 gint (*vfs_ungetc_impl)(gint c, VFSFile *stream) = NULL;
 gint (*vfs_fseek_impl)(VFSFile *file, glong offset, gint whence) = NULL;
@@ -53,10 +53,10 @@ vt_vfs_fclose_impl(VFSFile *file)
     return ret;
 }
 
-size_t
-vt_vfs_fread_impl(gpointer ptr, size_t size, size_t nmemb, VFSFile *file)
+gsize
+vt_vfs_fread_impl(gpointer ptr, gsize size, gsize nmemb, VFSFile *file)
 {
-    size_t ret;
+    gsize ret;
 
     ret = vfs_fread_impl(ptr, size, nmemb, file);
     g_print("%p fread       : size:%lu : nmemb:%lu : ret:%lu\n", file, size, nmemb, ret);
@@ -64,10 +64,10 @@ vt_vfs_fread_impl(gpointer ptr, size_t size, size_t nmemb, VFSFile *file)
     return ret;
 }
 
-size_t
-vt_vfs_fwrite_impl(gconstpointer ptr, size_t size, size_t nmemb, VFSFile *file)
+gsize
+vt_vfs_fwrite_impl(gconstpointer ptr, gsize size, gsize nmemb, VFSFile *file)
 {
-    size_t ret;
+    gsize ret;
 
     ret = vfs_fwrite_impl(ptr, size, nmemb, file);
     g_print("%p fwrite      : size:%lu : nmemb:%lu : ret:%lu\n", file, size, nmemb, ret);
