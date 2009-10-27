6c40f9132c65ad5f3a49e08fb5b370b7cbc80afd
William Pitcock <nenolod@atheme.org>
2009-10-27 02:07:04 -0500

    rocklight: Add support for the broadcom b43xxxx wireless activity (on/off) led, as seen on Dell laptops.

--- src/rocklight/b43led.c
+++ src/rocklight/b43led.c
@@ -1,5 +1,7 @@
 /*
- *  Copyright (C) 2004 Benedikt 'Hunz' Heinz <rocklight@hunz.org>
+ *  Copyright (C) 2006 Tony Vroon <chainsaw@gentoo.org>
+ *  Copyright (C) 2007 Michael Hanselmann <audacious@hansmi.ch>
+ *  Copyright (C) 2009 William Pitcock <nenolod@dereferenced.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,16 +18,24 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
  
-#ifndef _ROCKLIGHT_H
-#define _ROCKLIGHT_H
+#include <unistd.h>
+#include <stdlib.h>
 
-int thinklight_set(int fd, int state);
-int thinklight_get(int fd);
+#include "rocklight.h"
 
-int sysled_set(int fd, int state);
-int sysled_get(int fd);
+int b43led_set(int fd, int state) {
+	if (state) {
+		return write(fd, "0\n", 2);
+	} else {
+		return write(fd, "255\n", 4);
+	}
+}
 
-int b43led_set(int fd, int state);
-int b43led_get(int fd);
+int b43led_get(int fd) {
+	char buf[256];
+	int ret = read(fd, &buf, sizeof(buf));
+    if(!ret)
+        return 0; //dummy
 
-#endif
+	return (strtol(buf, NULL, 10) == 0);
+}
