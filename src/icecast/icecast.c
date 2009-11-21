2a21b850b865313495084cdae31198227cb7192e
John Lindgren <john.lindgren@tds.net>
2009-11-21 17:09:05 -0500

    icecast: Set type hint on configuration window (patch from AUDPLUG-131)

--- src/icecast/icecast.c
+++ src/icecast/icecast.c
@@ -609,6 +609,7 @@ static void ice_configure(void)
         plugin_new = plugin;
 
         configure_win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+        gtk_window_set_type_hint(GTK_WINDOW(configure_win), GDK_WINDOW_TYPE_HINT_DIALOG);
 
         gtk_signal_connect(GTK_OBJECT(configure_win), "destroy",
                            GTK_SIGNAL_FUNC(configure_destroy), NULL);
