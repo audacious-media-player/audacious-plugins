e30ffa0ae6f55a136fafd21f13354818b23539b4
John Lindgren <john.lindgren@tds.net>
2009-11-27 19:06:04 -0500

    filewriter: Use new libaudcore audio conversions

--- src/filewriter/convert.c
+++ src/filewriter/convert.c
@@ -1,64 +1,45 @@
 #include "convert.h"
 
-static SAD_dither_t *sad_state = NULL;
 gpointer convert_output = NULL;
-static gsize convert_output_length = 0;
 static gint nch;
 static AFormat in_fmt;
 static AFormat out_fmt;
 
 gboolean convert_init(AFormat input_fmt, AFormat output_fmt, gint channels)
 {
-    gint ret;
-    SAD_buffer_format input_sad_fmt;
-    SAD_buffer_format output_sad_fmt;
-
-
-    input_sad_fmt.sample_format = aud_sadfmt_from_afmt(input_fmt);
-    if (input_sad_fmt.sample_format < 0) return FALSE;
-    input_sad_fmt.fracbits = FMT_FRACBITS(input_fmt);
-    input_sad_fmt.channels = channels;
-    input_sad_fmt.channels_order = SAD_CHORDER_INTERLEAVED;
-    input_sad_fmt.samplerate = 0;
-
-    output_sad_fmt.sample_format = aud_sadfmt_from_afmt(output_fmt);
-    if (output_sad_fmt.sample_format < 0) return FALSE;
-    output_sad_fmt.fracbits = FMT_FRACBITS(output_fmt);
-    output_sad_fmt.channels = channels;
-    output_sad_fmt.channels_order = SAD_CHORDER_INTERLEAVED;
-    output_sad_fmt.samplerate = 0;
-
-    sad_state = SAD_dither_init(&input_sad_fmt, &output_sad_fmt, &ret);
-    if (sad_state == NULL) {
-        AUDDBG("ditherer init failed\n");
-        return FALSE;
-    }
-
     in_fmt = input_fmt;
     out_fmt = output_fmt;
     nch = channels;
 
-    SAD_dither_set_dither(sad_state, FALSE);
     return TRUE;
 }
 
 gint convert_process(gpointer ptr, gint length)
 {
-    gint frames, len;
-    frames = length / nch / FMT_SIZEOF(in_fmt);
-    len = frames * nch * FMT_SIZEOF(out_fmt);
+    gint samples = length / FMT_SIZEOF (in_fmt);
+    gfloat * temp;
 
-    if (convert_output == NULL || convert_output_length < len)
+    convert_output = g_realloc (convert_output, FMT_SIZEOF (out_fmt) * length);
+
+    if (in_fmt == out_fmt)
+        memcpy (convert_output, ptr, FMT_SIZEOF (in_fmt) * length);
+    else if (in_fmt == FMT_FLOAT)
+        audio_to_int (ptr, convert_output, out_fmt, length);
+    else if (out_fmt == FMT_FLOAT)
+        audio_from_int (ptr, in_fmt, convert_output, length);
+    else
     {
-        convert_output_length = len;
-        convert_output = aud_smart_realloc(convert_output, &convert_output_length);
+        temp = g_malloc (sizeof (gfloat) * length);
+        audio_from_int (ptr, in_fmt, temp, length);
+        audio_to_int (temp, convert_output, out_fmt, length);
+        g_free (temp);
     }
 
-    SAD_dither_process_buffer(sad_state, ptr, convert_output, frames);
-    return len;
+    return FMT_SIZEOF (out_fmt) * samples;
 }
 
 void convert_free(void)
 {
-    if (sad_state != NULL) {SAD_dither_free(sad_state); sad_state = NULL;}
+    g_free (convert_output);
+    convert_output = NULL;
 }
