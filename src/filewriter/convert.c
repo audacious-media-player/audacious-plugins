a038972b3d6a9075fec0123e933a7e3944ace408
John Lindgren <john.lindgren@tds.net>
2009-12-31 22:16:24 -0500

    filewriter: samples != length.

--- src/filewriter/convert.c
+++ src/filewriter/convert.c
@@ -19,19 +19,19 @@ gint convert_process(gpointer ptr, gint length)
     gint samples = length / FMT_SIZEOF (in_fmt);
     gfloat * temp;
 
-    convert_output = g_realloc (convert_output, FMT_SIZEOF (out_fmt) * length);
+    convert_output = g_realloc (convert_output, FMT_SIZEOF (out_fmt) * samples);
 
     if (in_fmt == out_fmt)
-        memcpy (convert_output, ptr, FMT_SIZEOF (in_fmt) * length);
+        memcpy (convert_output, ptr, FMT_SIZEOF (in_fmt) * samples);
     else if (in_fmt == FMT_FLOAT)
-        audio_to_int (ptr, convert_output, out_fmt, length);
+        audio_to_int (ptr, convert_output, out_fmt, samples);
     else if (out_fmt == FMT_FLOAT)
-        audio_from_int (ptr, in_fmt, convert_output, length);
+        audio_from_int (ptr, in_fmt, convert_output, samples);
     else
     {
-        temp = g_malloc (sizeof (gfloat) * length);
-        audio_from_int (ptr, in_fmt, temp, length);
-        audio_to_int (temp, convert_output, out_fmt, length);
+        temp = g_malloc (sizeof (gfloat) * samples);
+        audio_from_int (ptr, in_fmt, temp, samples);
+        audio_to_int (temp, convert_output, out_fmt, samples);
         g_free (temp);
     }
 
