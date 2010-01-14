9d623fa9387b8f5cd0751708b0cb865ab3d801c9
Tony Vroon <chainsaw@gentoo.org>
2010-01-14 21:53:54 +0000

    Do not supply a bare variable where a format string is expected.

--- src/skins/plugin.c
+++ src/skins/plugin.c
@@ -255,7 +255,7 @@ static void show_error_message(const gchar * markup)
                                            GTK_DIALOG_DESTROY_WITH_PARENT,
                                            GTK_MESSAGE_ERROR,
                                            GTK_BUTTONS_OK,
-                                           _(markup));
+                                           "%s",_(markup));
 
     gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
     gtk_widget_show(GTK_WIDGET(dialog));
