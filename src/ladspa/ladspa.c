4a48193b771596de27be6e74abc84d79f727a521
Tony Vroon <chainsaw@gentoo.org>
2010-03-23 22:49:23 +0000

    Backed out changeset c56fa227f8db as a fix has been proposed.

--- src/ladspa/ladspa.c
+++ src/ladspa/ladspa.c
@@ -76,6 +76,7 @@ static void restore(void);
 static plugin_instance *add_plugin(ladspa_plugin * plugin);
 static void find_all_plugins(void);
 static void find_plugins(char *path_entry);
+static ladspa_plugin *get_plugin_by_id(const gchar * basename, long id);
 static plugin_instance *load(char *filename, long int num);
 static void reboot_plugins(void);
 static void boot_plugin(plugin_instance * instance);
@@ -144,7 +145,72 @@ static void start(void)
 
 static void restore(void)
 {
+#if 0
+    mcs_handle_t *db;
+    gint k, plugins = 0;
+
+    db = aud_cfg_db_open();
+
+    aud_cfg_db_get_int(db, "ladspa", "plugins", &plugins);
+    for (k = 0; k < plugins; ++k)
+    {
+	gint id;
+	int port, ports = 0;
+	plugin_instance *instance;
+	gchar *bn, *section;
+
+	bn = g_path_get_basename(instance->filename);
+	section = g_strdup_printf("ladspa_plugin:%s:%d", bn, k);
+	g_free(bn);
+
+	aud_cfg_db_get_int(db, section, "id", &id);
+	aud_cfg_db_get_int(db, section, "ports", &ports);
+
+	instance = add_plugin(get_plugin_by_id(id));
+	if (!instance)
+	    continue;		/* couldn't load this plugin */
+
+	for (port = 0; port < ports && port < MAX_KNOBS; ++port)
+	{
+	    gchar *key = g_strdup_printf("port%d", port);
+	    aud_cfg_db_get_float(db, section, key, &(instance->knobs[port]));
+	}
+	instance->restored = TRUE;
+	g_free(section);
+    }
+
+    aud_cfg_db_close(db);
+#endif
+
     state.initialised = TRUE;
+}
+
+static ladspa_plugin *get_plugin_by_id(const gchar * basename, long id)
+{
+    GSList *list;
+    ladspa_plugin *plugin;
+
+    if (plugin_list == NULL)
+    {
+	find_all_plugins();
+    }
+
+    for (list = plugin_list; list != NULL; list = g_slist_next(list))
+    {
+	gchar *bn;
+	plugin = (ladspa_plugin *) list->data;
+
+	bn = g_path_get_basename(plugin->filename);
+	if (plugin->unique_id == id && !g_ascii_strcasecmp(basename, bn))
+	{
+	    g_free(bn);
+	    return plugin;
+	}
+
+	g_free(bn);
+    }
+
+    return NULL;
 }
 
 static void find_all_plugins(void)
