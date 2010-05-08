1d78ee5b955b6c1c8a5fffef9ccab03bbeebc773
William Pitcock <nenolod@atheme.org>
2010-05-08 02:04:09 -0500

    Import skeleton of new mad plugin.  Lots to do.

---
+++ src/mad/mad_decode.c
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2010 William Pitcock <nenolod@dereferenced.org>.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <audacious/plugin.h>
+#include <mad.h>
+
+typedef struct {
+	VFSFile *fd;
+} MADDecodeContext;
+
+#define BUFFER_SIZE		(1024)
+
+static enum mad_flow
+madplug_input(InputPlayback *ip, struct mad_stream *stream)
+{
+	MADDecodeContext *ctx = (MADDecodeContext *) ip->data;
+	guchar buffer[BUFFER_SIZE];
+	gsize len;
+
+	if (!(len = aud_vfs_fread(buffer, 1, BUFFER_SIZE, ctx->fd)))
+		return MAD_FLOW_STOP;
+
+	mad_stream_buffer(stream, buffer, len);
+
+	return MAD_FLOW_CONTINUE;
+}
+
+static enum mad_flow
+madplug_header(InputPlayback *ip, const struct mad_header *header)
+{
+	if (!ip->output->open_audio(FMT_FLOAT, header->samplerate, MAD_NCHANNELS(header)))
+		return MAD_FLOW_STOP;
+
+	ip->set_params(ip, NULL, 0, header->samplerate, MAD_NCHANNELS(header), header->bitrate);
+
+	return MAD_FLOW_CONTINUE;
+}
+
+static enum mad_flow
+madplug_output(InputPlayback *ip, const struct mad_header *header, struct mad_pcm *pcm)
+{
+	gint channels = MAD_NCHANNELS(header);
+	gfloat *data = g_malloc (sizeof(gfloat) * channels * pcm->length);
+	gfloat *end = data + channels * pcm->length;
+	gint channel;
+
+	for (channel = 0; channel < channels; channel++)
+	{
+		const mad_fixed_t *from = pcm->samples[channel];
+		gfloat *to = data + channel;
+
+		while (to < end)
+		{
+			*to = (gfloat) (*from++) / (1 << 28);
+			to += channels;
+		}
+	}
+
+	ip->pass_audio(ip, FMT_FLOAT, channels, sizeof(gfloat) * channels * pcm->length, data, NULL);
+	g_free(data);
+
+	return MAD_FLOW_CONTINUE;
+}
+
+static enum mad_flow
+madplug_error(InputPlayback *ip, struct mad_stream *stream, struct mad_frame *frame)
+{
+	return MAD_FLOW_CONTINUE;
+}
+
+void
+madplug_decode(InputPlayback *ip, VFSFile *fd)
+{
+	MADDecodeContext ctx;
+	struct mad_decoder decoder;
+
+	ctx.fd = aud_vfs_dup(fd);
+
+	mad_decoder_init(&decoder, &ctx, (void *) madplug_input, (void *) madplug_header, NULL, (void *) madplug_output, (void *) madplug_error, NULL);	
+	mad_decoder_run(&decoder, MAD_DECODER_MODE_SYNC);
+	mad_decoder_finish(&decoder);
+
+	aud_vfs_fclose(ctx.fd);
+}
