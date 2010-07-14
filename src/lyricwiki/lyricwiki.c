a4ee74dfe9b39785b44a8f5c4810c8427c481a69
William Pitcock <nenolod@atheme.org>
2010-07-14 04:12:30 -0500

    Add lyricwiki viewer plugin: - Hacked up in about 10 minutes. - Awful code. - You don't want to use this. - At least it doesn't require some bullshit ruby script like all the other ones.

---
+++ src/lyricwiki/lyricwiki.c
@@ -0,0 +1,334 @@
+/*
+ * Copyright (c) 2010 William Pitcock <nenolod@dereferenced.org>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * TODO:
+ *   - do the VFS operations in another thread
+ *   - why don't we have a url encoding function?
+ *     ... or do we and I'm just smoking crack?
+ *   - no idea what this code does if lyricwiki is down - probably crashes!
+ */
+
+#include <stdio.h>
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/xpath.h>
+#include <mowgli.h>
+
+#include <audacious/plugin.h>
+
+#include "urlencode.h"
+
+/*
+ * Suppress libxml warnings, because lyricwiki does not generate anything near
+ * valid HTML.
+ */
+void
+libxml_error_handler(void *ctx, const char *msg, ...)
+{
+
+}
+
+/* Oh, this is going to be fun... */
+gchar *
+scrape_lyrics_from_lyricwiki_edit_page(const gchar *buf, gsize len)
+{
+	xmlDocPtr doc;
+	gchar *ret = NULL;
+
+	/* 
+	 * temporarily set our error-handling functor to our suppression function,
+	 * but we have to set it back because other components of Audacious depend
+	 * on libxml and we don't want to step on their code paths.
+	 *
+	 * unfortunately, libxml is anti-social and provides us with no way to get
+	 * the previous error functor, so we just have to set it back to default after
+	 * parsing and hope for the best.
+	 */
+	xmlSetGenericErrorFunc(NULL, libxml_error_handler);
+	doc = htmlReadMemory(buf, (int) len, NULL, "utf-8", (HTML_PARSE_RECOVER | HTML_PARSE_NONET));
+	xmlSetGenericErrorFunc(NULL, NULL);
+
+	if (doc != NULL)
+	{
+		xmlXPathContextPtr xpath_ctx = NULL;
+		xmlXPathObjectPtr xpath_obj = NULL;
+		xmlNodePtr node = NULL;
+
+		xpath_ctx = xmlXPathNewContext(doc);
+		if (xpath_ctx == NULL)
+			goto give_up;
+
+		xpath_obj = xmlXPathEvalExpression((xmlChar *) "//*[@id=\"wpTextbox1\"]", xpath_ctx);
+		if (xpath_obj == NULL)
+			goto give_up;
+
+		if (!xpath_obj->nodesetval->nodeMax)
+			goto give_up;
+
+		node = xpath_obj->nodesetval->nodeTab[0];
+give_up:
+		if (xpath_obj != NULL)
+			xmlXPathFreeObject(xpath_obj);
+
+		if (xpath_ctx != NULL)
+			xmlXPathFreeContext(xpath_ctx);
+
+		if (node != NULL)
+		{
+			xmlChar *lyric = xmlNodeGetContent(node);
+
+			if (lyric != NULL)
+			{
+				GMatchInfo *match_info;
+				GRegex *reg;
+
+				reg = g_regex_new("<(lyrics?)>(.*)</\\1>", (G_REGEX_MULTILINE | G_REGEX_DOTALL), 0, NULL);
+				g_regex_match(reg, (gchar *) lyric, G_REGEX_MATCH_NEWLINE_ANY, &match_info);
+
+				ret = g_match_info_fetch(match_info, 2);
+				if (!g_utf8_collate(ret, "\n\n<!-- PUT LYRICS HERE (and delete this entire line) -->\n\n"))
+				{
+					g_free(ret);
+					ret = NULL;
+				}
+			}
+
+			xmlFree(lyric);
+		}
+	}
+
+	return ret;
+}
+
+gchar *
+scrape_uri_from_lyricwiki_search_result(const gchar *buf, gsize len)
+{
+	xmlDocPtr doc;
+	gchar *uri = NULL;
+
+	/* 
+	 * temporarily set our error-handling functor to our suppression function,
+	 * but we have to set it back because other components of Audacious depend
+	 * on libxml and we don't want to step on their code paths.
+	 *
+	 * unfortunately, libxml is anti-social and provides us with no way to get
+	 * the previous error functor, so we just have to set it back to default after
+	 * parsing and hope for the best.
+	 */
+	xmlSetGenericErrorFunc(NULL, libxml_error_handler);
+	doc = xmlParseMemory(buf, (int) len);
+	xmlSetGenericErrorFunc(NULL, NULL);
+
+	if (doc != NULL)
+	{
+		xmlNodePtr root, cur;
+
+		root = xmlDocGetRootElement(doc);
+
+		MOWGLI_ITER_FOREACH(cur, root->xmlChildrenNode)
+		{
+			if (xmlStrEqual(cur->name, (xmlChar *) "url"))
+			{
+				xmlChar *lyric;
+				gchar *basename;
+
+				lyric = xmlNodeGetContent(cur);
+				basename = g_path_get_basename((gchar *) lyric);
+
+				uri = g_strdup_printf("http://lyrics.wikia.com/index.php?action=edit&title=%s", basename);
+				g_free(basename);
+				xmlFree(lyric);
+			}
+		}
+	}
+
+	return uri;
+}
+
+gchar *
+get_lyrics_uri_for_song(const Tuple *tu)
+{
+	gchar *uri, *buf, *ret;
+	gchar *artist, *title;
+	gint64 len;
+
+	artist = lyricwiki_url_encode(tuple_get_string(tu, FIELD_ARTIST, NULL));
+	title = lyricwiki_url_encode(tuple_get_string(tu, FIELD_TITLE, NULL));
+
+	uri = g_strdup_printf("http://lyrics.wikia.com/api.php?action=lyrics&artist=%s&song=%s&fmt=xml", artist, title);
+
+	g_free(artist);
+	g_free(title);
+
+	vfs_file_get_contents(uri, (gpointer) &buf, &len);
+
+	g_free(uri);
+
+	ret = scrape_uri_from_lyricwiki_search_result(buf, len);
+
+	g_free(buf);
+
+	return ret;
+}
+
+GtkWidget *window, *textview;
+GtkTextBuffer *textbuffer;
+
+GtkWidget *
+build_widget(void)
+{
+	GtkWidget *scrollview, *vbox;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), _("LyricWiki"));
+	gtk_window_set_default_size(GTK_WINDOW(window), 300, 500);
+
+	textview = gtk_text_view_new();
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
+	gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(textview), FALSE);
+	gtk_text_view_set_left_margin(GTK_TEXT_VIEW(textview), 12);
+	gtk_text_view_set_right_margin(GTK_TEXT_VIEW(textview), 12);
+	textbuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
+	
+	scrollview = gtk_scrolled_window_new(NULL, NULL);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollview), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	vbox = gtk_vbox_new(FALSE, 10);
+
+	gtk_container_add(GTK_CONTAINER(scrollview), textview);
+
+	gtk_box_pack_start(GTK_BOX(vbox), scrollview, TRUE, TRUE, 0);
+
+	gtk_container_add(GTK_CONTAINER(window), vbox);
+
+	gtk_widget_show(textview);
+	gtk_widget_show(scrollview);
+	gtk_widget_show(vbox);
+	gtk_widget_show(window);
+
+	gtk_text_buffer_create_tag(GTK_TEXT_BUFFER(textbuffer), "weight_bold", "weight", PANGO_WEIGHT_BOLD, NULL); 
+	gtk_text_buffer_create_tag(GTK_TEXT_BUFFER(textbuffer), "size_x_large", "scale", PANGO_SCALE_X_LARGE, NULL);
+	gtk_text_buffer_create_tag(GTK_TEXT_BUFFER(textbuffer), "style_italic", "style", PANGO_STYLE_ITALIC, NULL);
+
+	return window;
+}
+
+void
+clear_lyrics_window(void)
+{
+	GtkTextIter iter1, iter2;
+
+	gtk_text_buffer_get_start_iter(GTK_TEXT_BUFFER(textbuffer), &iter1);
+	gtk_text_buffer_get_end_iter(GTK_TEXT_BUFFER(textbuffer), &iter2);
+
+	gtk_text_buffer_delete(GTK_TEXT_BUFFER(textbuffer), &iter1, &iter2);
+}
+
+void
+update_lyrics_window(const Tuple *tu, const gchar *lyrics)
+{
+	GtkTextIter iter;
+	const gchar *artist, *title;
+
+	gtk_text_buffer_get_start_iter(GTK_TEXT_BUFFER(textbuffer), &iter);
+
+	title = tuple_get_string(tu, FIELD_TITLE, NULL);
+	artist = tuple_get_string(tu, FIELD_ARTIST, NULL);
+
+	gtk_text_buffer_insert_with_tags_by_name(GTK_TEXT_BUFFER(textbuffer), &iter, 
+			title, strlen(title), "weight_bold", "size_x_large", NULL);
+
+	gtk_text_buffer_insert(GTK_TEXT_BUFFER(textbuffer), &iter, "\n", 1);
+
+	gtk_text_buffer_insert_with_tags_by_name(GTK_TEXT_BUFFER(textbuffer), &iter,
+			artist, strlen(artist), "style_italic", NULL);
+
+	gtk_text_buffer_insert(GTK_TEXT_BUFFER(textbuffer), &iter, "\n", 1);
+	gtk_text_buffer_insert(GTK_TEXT_BUFFER(textbuffer), &iter, lyrics, strlen(lyrics));
+}
+
+void
+lyricwiki_playback_began(void)
+{
+	gint playlist, pos;
+	const Tuple *tu;
+	gchar *uri, *raw_lyrics, *lyrics;
+	gint64 len;
+
+	if (!audacious_drct_is_playing())
+		return;
+
+	clear_lyrics_window();
+
+	playlist = aud_playlist_get_active();
+	pos = aud_playlist_get_position(playlist);
+	tu = aud_playlist_entry_get_tuple(playlist, pos);
+
+	uri = get_lyrics_uri_for_song(tu);
+	if (uri == NULL)
+		goto error;
+
+	vfs_file_get_contents(uri, (void *) &raw_lyrics, &len);
+	if (raw_lyrics == NULL)
+		goto error;
+
+	lyrics = scrape_lyrics_from_lyricwiki_edit_page(raw_lyrics, len);
+	g_free(raw_lyrics);
+
+	if (lyrics == NULL)
+		goto error;
+
+	update_lyrics_window(tu, lyrics);
+	g_free(lyrics);
+error:
+	g_free(uri);
+}
+
+static void
+init(void)
+{
+	aud_hook_associate("playback begin", (HookFunction) lyricwiki_playback_began, NULL);
+
+	build_widget();
+}
+
+static void
+cleanup(void)
+{
+	aud_hook_dissociate("playback begin", (HookFunction) lyricwiki_playback_began);
+
+	gtk_widget_destroy(window);
+	window = NULL;
+}
+
+GeneralPlugin lyricwiki =
+{
+	.description = "LyricWiki",
+	.init = init,
+	.cleanup = cleanup,
+};
+
+GeneralPlugin *lyricwiki_gplist[] = { &lyricwiki, NULL };
+SIMPLE_GENERAL_PLUGIN(lyricwiki, lyricwiki_gplist);
