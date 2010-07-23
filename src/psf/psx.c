4fd268872e16b8e303bcef787f4cd08b1b3c8056
Tony Vroon <chainsaw@gentoo.org>
2010-07-23 23:51:09 +0100

    psf: Remove (now) unused delayn array.

--- src/psf/psx.c
+++ src/psf/psx.c
@@ -154,15 +154,6 @@ static UINT8 mips_win_layout[] = {
 	 0,23,80, 1 	/* command line window (bottom rows) */
 };
 
-static const char *delayn[] =
-{
-	"pc", "at", "v0", "v1", "a0", "a1", "a2", "a3",
-	"t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
-	"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
-	"t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra",
-	"pc"
-};
-
 #define REGPC ( 32 )
 
 typedef struct
