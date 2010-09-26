716cfad91d4f9207c9076958a8b29f152273bba3
William Pitcock <nenolod@atheme.org>
2010-09-25 22:36:59 -0500

    eng_protos.h is actually just headers for the plugin itself... not allegedly part of MAME...

--- src/psf/eng_protos.h
+++ src/psf/eng_protos.h
@@ -1,15 +1,4 @@
-//
-// Audio Overload
-// Emulated music player
-//
-// (C) 2000-2007 Richard F. Bannister
-//
-
 #include <audacious/plugin.h>
-
-//
-// eng_protos.h
-//
 
 int32 psf2_start(uint8 *, uint32 length);
 int32 psf2_execute(InputPlayback *playback);
