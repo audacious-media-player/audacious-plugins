5b5dcfca30e3d9c50b074d878a81fc9f49d8a5ce
John Lindgren <john.lindgren@tds.net>
2011-02-16 15:58:57 -0500

    mpg123: Import upstream rev. 2843 (unused variable).

--- src/mpg123/libmpg123/format.c
+++ src/mpg123/libmpg123/format.c
@@ -426,6 +426,7 @@ off_t bytes_to_samples(mpg123_handle *fr , off_t b)
 }
 
 
+#ifndef NO_32BIT
 /* Remove every fourth byte, facilitating conversion from 32 bit to 24 bit integers.
    This has to be aware of endianess, of course. */
 static void chop_fourth_byte(struct outbuffer *buf)
@@ -459,6 +460,7 @@ static void chop_fourth_byte(struct outbuffer *buf)
 #endif
 	buf->fill = wpos-buf->data;
 }
+#endif
 
 void postprocess_buffer(mpg123_handle *fr)
 {
