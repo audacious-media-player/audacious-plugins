e61ff2aea727d41f78717e5f1d058c8ac1a28ad9
William Pitcock <nenolod@atheme.org>
2011-02-22 12:14:35 -0600

    lyricwiki: smaller margins

--- src/lyricwiki/lyricwiki.c
+++ src/lyricwiki/lyricwiki.c
@@ -260,8 +260,8 @@ build_widget(void)
 	textview = gtk_text_view_new();
 	gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
 	gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(textview), FALSE);
-	gtk_text_view_set_left_margin(GTK_TEXT_VIEW(textview), 12);
-	gtk_text_view_set_right_margin(GTK_TEXT_VIEW(textview), 12);
+	gtk_text_view_set_left_margin(GTK_TEXT_VIEW(textview), 4);
+	gtk_text_view_set_right_margin(GTK_TEXT_VIEW(textview), 4);
 	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview), GTK_WRAP_WORD);
 	textbuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
 
