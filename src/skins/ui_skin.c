869623b61604085f95af56ef441e19a1e85866d9
John Lindgren <john.lindgren@tds.net>
2011-06-04 23:48:47 -0400

    skins: Fix invalid memory access when setting config.skin to ... config.skin.

--- src/skins/ui_skin.c
+++ src/skins/ui_skin.c
@@ -154,8 +154,12 @@ gboolean active_skin_load (const gchar * path)
     gtk_widget_queue_draw (equalizerwin);
     gtk_widget_queue_draw (playlistwin);
 
-    g_free (config.skin);
-    config.skin = g_strdup (path);
+    /* path may in fact be the same string as config.skin */
+    if (strcmp (path, config.skin))
+    {
+        g_free (config.skin);
+        config.skin = g_strdup (path);
+    }
 
     return TRUE;
 }
