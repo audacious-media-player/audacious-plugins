f8be7713e843bc496c6dfbc2a74adb4e0cc3e258
John Lindgren <john.lindgren@tds.net>
2011-06-06 19:35:36 -0400

    skins: Fix null pointer dereference.

--- src/skins/ui_skin.c
+++ src/skins/ui_skin.c
@@ -155,7 +155,7 @@ gboolean active_skin_load (const gchar * path)
     gtk_widget_queue_draw (playlistwin);
 
     /* path may in fact be the same string as config.skin */
-    if (strcmp (path, config.skin))
+    if (! config.skin || strcmp (path, config.skin))
     {
         g_free (config.skin);
         config.skin = g_strdup (path);
