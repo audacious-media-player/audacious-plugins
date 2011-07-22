d95f7461c66590ef69528f346645617e36db3485
John Lindgren <john.lindgren@tds.net>
2011-07-22 09:45:01 -0400

    ladspa: Add copyright notice and about window.

--- src/ladspa/plugin.h
+++ src/ladspa/plugin.h
@@ -1,3 +1,22 @@
+/*
+ * LADSPA Host for Audacious
+ * Copyright 2011 John Lindgren
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions, and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions, and the following disclaimer in the documentation
+ *    provided with the distribution.
+ *
+ * This software is provided "as is" and without any warranty, express or
+ * implied. In no event shall the authors be liable for any damages arising from
+ * the use of this software.
+ */
+
 #ifndef AUD_LADSPA_PLUGIN_H
 #define AUD_LADSPA_PLUGIN_H
 
@@ -45,6 +64,7 @@ extern struct index * modules; /* (void *) returned by dlopen() */
 extern struct index * plugins; /* (PluginData *) */
 extern struct index * loadeds; /* (LoadedPlugin *) */
 
+extern GtkWidget * about_win;
 extern GtkWidget * config_win;
 extern GtkWidget * plugin_list;
 extern GtkWidget * loaded_list;
