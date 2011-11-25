b17f293c9dc655d6bad3fde3978fb436d0a1a09b
John Lindgren <john.lindgren@tds.net>
2011-11-25 16:14:46 -0500

    Add beginnings of library search plugin.

---
+++ src/search-tool/search-tool.c
@@ -0,0 +1,126 @@
+#include "config.h"
+
+#include <glib.h>
+
+#include <audacious/i18n.h>
+#include <audacious/misc.h>
+#include <audacious/playlist.h>
+#include <audacious/plugin.h>
+#include <libaudcore/audstrings.h>
+
+static GHashTable * added_table = NULL;
+static gint playlist_id;
+
+static void find_playlist (void)
+{
+    playlist_id = -1;
+
+    for (gint i = 0; i < aud_playlist_count (); i ++)
+    {
+        gchar * title = aud_playlist_get_title (i);
+
+        if (! strcmp (title, _("Library")))
+        {
+            playlist_id = aud_playlist_get_unique_id (i);
+            break;
+        }
+
+        g_free (title);
+    }
+}
+
+static void create_playlist (void)
+{
+    aud_playlist_insert (0);
+    aud_playlist_set_title (0, _("Library"));
+    playlist_id = aud_playlist_get_unique_id (0);
+}
+
+static gint get_playlist (void)
+{
+    if (aud_playlist_by_unique_id (playlist_id) < 0)
+        create_playlist ();
+
+    return aud_playlist_by_unique_id (playlist_id);
+}
+
+static gchar * get_path (void)
+{
+    gchar * path = aud_get_string ("search-tool", "path");
+    if (g_file_test (path, G_FILE_TEST_EXISTS))
+        goto FOUND;
+
+    g_free (path);
+    path = g_build_filename (g_get_home_dir (), "Music", NULL);
+    if (g_file_test (path, G_FILE_TEST_EXISTS))
+        goto FOUND;
+
+    g_free (path);
+    path = g_strdup (g_get_home_dir ());
+
+FOUND:
+    aud_set_string ("search-tool", "path", path);
+    return path;
+}
+
+static void destroy_added_table (void)
+{
+    if (! added_table)
+        return;
+
+    g_hash_table_destroy (added_table);
+    added_table = NULL;
+}
+
+static void create_added_table (gint list)
+{
+    destroy_added_table ();
+    added_table = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
+
+    gint entries = aud_playlist_entry_count (list);
+    for (gint i = 0; i < entries; i ++)
+        g_hash_table_insert (added_table, aud_playlist_entry_get_filename (list, i), NULL);
+}
+
+static gboolean filter_cb (const gchar * filename, void * unused)
+{
+    return added_table && ! g_hash_table_lookup_extended (added_table, filename, NULL, NULL);
+}
+
+static void begin_scan (void)
+{
+    gint list = get_playlist ();
+    gchar * path = get_path ();
+
+    aud_playlist_remove_failed (list);
+
+    create_added_table (list);
+
+    struct index * add = index_new ();
+    index_append (add, filename_to_uri (path));
+    aud_playlist_entry_insert_filtered (list, -1, add, NULL, filter_cb, NULL, FALSE);
+
+    g_free (path);
+}
+
+static gboolean search_init (void)
+{
+    find_playlist ();
+
+    /* temporary ... */
+    begin_scan ();
+
+    return TRUE;
+}
+
+void search_cleanup (void)
+{
+    destroy_added_table ();
+}
+
+AUD_GENERAL_PLUGIN
+(
+    .name = "Search Tool",
+    .init = search_init,
+    .cleanup = search_cleanup,
+)
