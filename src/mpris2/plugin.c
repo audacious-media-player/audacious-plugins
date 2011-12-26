868b06c2e1e50a0797294b3209eef7c3a35d9ad1
John Lindgren <john.lindgren@aol.com>
2011-12-26 13:37:21 -0500

    mpris2: Add basic player functions.

--- src/mpris2/plugin.c
+++ src/mpris2/plugin.c
@@ -5,11 +5,13 @@
 #include <audacious/drct.h>
 #include <audacious/misc.h>
 #include <audacious/plugin.h>
+#include <libaudcore/hook.h>
 
 #include "object-core.h"
+#include "object-player.h"
 
 static GDBusConnection * bus;
-static GObject * object_core;
+static GObject * object_core, * object_player;
 
 static bool_t quit_cb (MprisMediaPlayer2 * object, GDBusMethodInvocation * call,
  void * unused)
@@ -25,6 +27,88 @@ static bool_t raise_cb (MprisMediaPlayer2 * object, GDBusMethodInvocation *
     aud_interface_show (TRUE);
     mpris_media_player2_complete_raise (object, call);
     return TRUE;
+}
+
+static void update_playback_status (void * data, GObject * object)
+{
+    const char * status;
+
+    if (aud_drct_get_playing ())
+        status = aud_drct_get_paused () ? "Paused" : "Playing";
+    else
+        status = "Stopped";
+
+    g_object_set (object, "playback-status", status, NULL);
+}
+
+static bool_t next_cb (MprisMediaPlayer2Player * object, GDBusMethodInvocation *
+ call, void * unused)
+{
+    aud_drct_pl_next ();
+    mpris_media_player2_player_complete_next (object, call);
+    return TRUE;
+}
+
+static bool_t pause_cb (MprisMediaPlayer2Player * object,
+ GDBusMethodInvocation * call, void * unused)
+{
+    if (aud_drct_get_playing () && ! aud_drct_get_paused ())
+        aud_drct_pause ();
+
+    mpris_media_player2_player_complete_pause (object, call);
+    return TRUE;
+}
+
+static bool_t play_cb (MprisMediaPlayer2Player * object, GDBusMethodInvocation *
+ call, void * unused)
+{
+    if (! aud_drct_get_playing () || aud_drct_get_paused ())
+        aud_drct_play ();
+
+    mpris_media_player2_player_complete_play (object, call);
+    return TRUE;
+}
+
+static bool_t play_pause_cb (MprisMediaPlayer2Player * object,
+ GDBusMethodInvocation * call, void * unused)
+{
+    if (aud_drct_get_playing () && ! aud_drct_get_paused ())
+        aud_drct_pause ();
+    else
+        aud_drct_play ();
+
+    mpris_media_player2_player_complete_play_pause (object, call);
+    return TRUE;
+}
+
+static bool_t previous_cb (MprisMediaPlayer2Player * object,
+ GDBusMethodInvocation * call, void * unused)
+{
+    aud_drct_pl_prev ();
+    mpris_media_player2_player_complete_previous (object, call);
+    return TRUE;
+}
+
+static bool_t stop_cb (MprisMediaPlayer2Player * object, GDBusMethodInvocation *
+ call, void * unused)
+{
+    if (aud_drct_get_playing ())
+        aud_drct_stop ();
+
+    mpris_media_player2_player_complete_stop (object, call);
+    return TRUE;
+}
+
+void mpris2_cleanup (void)
+{
+    hook_dissociate ("playback begin", (HookFunction) update_playback_status);
+    hook_dissociate ("playback pause", (HookFunction) update_playback_status);
+    hook_dissociate ("playback stop", (HookFunction) update_playback_status);
+    hook_dissociate ("playback unpause", (HookFunction) update_playback_status);
+
+    g_dbus_connection_close_sync (bus, NULL, NULL);
+    g_object_unref (object_core);
+    g_object_unref (object_player);
 }
 
 bool_t mpris2_init (void)
@@ -55,22 +139,43 @@ bool_t mpris2_init (void)
     g_signal_connect (object_core, "handle-quit", (GCallback) quit_cb, NULL);
     g_signal_connect (object_core, "handle-raise", (GCallback) raise_cb, NULL);
 
+    object_player = (GObject *) mpris_media_player2_player_skeleton_new ();
+
+    g_object_set (object_player,
+     "can-control", TRUE,
+     "can-go-next", TRUE,
+     "can-go-previous", TRUE,
+     "can-pause", TRUE,
+     "can-play", TRUE,
+     "can-seek", FALSE,
+     NULL);
+
+    update_playback_status (NULL, object_player);
+
+    hook_associate ("playback begin", (HookFunction) update_playback_status, object_player);
+    hook_associate ("playback pause", (HookFunction) update_playback_status, object_player);
+    hook_associate ("playback stop", (HookFunction) update_playback_status, object_player);
+    hook_associate ("playback unpause", (HookFunction) update_playback_status, object_player);
+
+    g_signal_connect (object_player, "handle-next", (GCallback) next_cb, NULL);
+    g_signal_connect (object_player, "handle-pause", (GCallback) pause_cb, NULL);
+    g_signal_connect (object_player, "handle-play", (GCallback) play_cb, NULL);
+    g_signal_connect (object_player, "handle-play-pause", (GCallback) play_pause_cb, NULL);
+    g_signal_connect (object_player, "handle-previous", (GCallback) previous_cb, NULL);
+    g_signal_connect (object_player, "handle-stop", (GCallback) stop_cb, NULL);
+
     if (! g_dbus_interface_skeleton_export ((GDBusInterfaceSkeleton *)
-     object_core, bus, "/org/mpris/MediaPlayer2", & error))
+     object_core, bus, "/org/mpris/MediaPlayer2", & error) ||
+     ! g_dbus_interface_skeleton_export ((GDBusInterfaceSkeleton *)
+     object_player, bus, "/org/mpris/MediaPlayer2", & error))
     {
-        g_dbus_connection_close_sync (bus, NULL, NULL);
+        mpris2_cleanup ();
         fprintf (stderr, "mpris2: %s\n", error->message);
         g_error_free (error);
         return FALSE;
     }
 
     return TRUE;
-}
-
-void mpris2_cleanup (void)
-{
-    g_dbus_connection_close_sync (bus, NULL, NULL);
-    g_object_unref (object_core);
 }
 
 AUD_GENERAL_PLUGIN
