5f1610f88b16b2fe24688d257952a1745ab5fc55
John Lindgren <john.lindgren@aol.com>
2012-03-31 12:04:53 -0400

    sid: Use GLib MD5 implementation.

--- src/sid/xs_md5.h
+++ src/sid/xs_md5.h
@@ -1,34 +1,21 @@
+/* Wrapper for GLib MD5 implementation */
+/* John Lindgren, March 31, 2012 */
+/* Public domain */
+
 #ifndef XS_MD5_H
 #define XS_MD5_H
 
-#include <glib.h>
+#define XS_MD5HASH_LENGTH 16
+#define XS_MD5HASH_LENGTH_CH 32
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/* Typedefs
- */
-typedef struct md5_state_s {
-    guint32 bits[2];    /* message length in bits, lsw first */
-    guint32 buf[4];     /* digest buffer */
-    guint8 in[64];      /* accumulate block */
+typedef struct {
+    void * priv;
 } xs_md5state_t;
 
-#define XS_MD5HASH_LENGTH       (16)
-#define XS_MD5HASH_LENGTH_CH    (XS_MD5HASH_LENGTH * 2)
+typedef unsigned char xs_md5hash_t[XS_MD5HASH_LENGTH];
 
-typedef guint8 xs_md5hash_t[XS_MD5HASH_LENGTH];
+void xs_md5_init (xs_md5state_t * state);
+void xs_md5_append (xs_md5state_t * state, const void * data, int length);
+void xs_md5_finish (xs_md5state_t * state, xs_md5hash_t hash);
 
-
-/* Functions
- */
-void xs_md5_init(xs_md5state_t *ctx);
-void xs_md5_append(xs_md5state_t *ctx, const guint8 *buf, guint len);
-void xs_md5_finish(xs_md5state_t *ctx, xs_md5hash_t digest);
-
-
-#ifdef __cplusplus
-}
 #endif
-#endif /* XS_MD5_H */
