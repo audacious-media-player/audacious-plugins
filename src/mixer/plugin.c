15725a43916b6c81d78d32f4aecae46397354d9c
John Lindgren <john.lindgren@aol.com>
2012-06-02 14:25:47 -0400

    mixer: Use about_text.

--- src/mixer/plugin.c
+++ src/mixer/plugin.c
@@ -35,7 +35,7 @@ static const gchar * const mixer_defaults[] = {
 int mixer_channels;
 float * mixer_buf;
 
-static GtkWidget * about_win, * config_win;
+static GtkWidget * config_win;
 
 static int mixer_init (void)
 {
@@ -46,8 +46,6 @@ static int mixer_init (void)
 
 static void mixer_cleanup (void)
 {
-    if (about_win)
-        gtk_widget_destroy (about_win);
     if (config_win)
         gtk_widget_destroy (config_win);
 
@@ -55,25 +53,6 @@ static void mixer_cleanup (void)
 
     free (mixer_buf);
     mixer_buf = 0;
-}
-
-static void mixer_about (void)
-{
-    audgui_simple_message (& about_win, GTK_MESSAGE_INFO,
-     _("About Channel Mixer"),
-     "Channel Mixer Plugin for Audacious\n"
-     "Copyright 2011 John Lindgren\n\n"
-     "Redistribution and use in source and binary forms, with or without "
-     "modification, are permitted provided that the following conditions are "
-     "met:\n\n"
-     "1. Redistributions of source code must retain the above copyright "
-     "notice, this list of conditions, and the following disclaimer.\n\n"
-     "2. Redistributions in binary form must reproduce the above copyright "
-     "notice, this list of conditions, and the following disclaimer in the "
-     "documentation provided with the distribution.\n\n"
-     "This software is provided \"as is\" and without any warranty, express or "
-     "implied. In no event shall the authors be liable for any damages arising "
-     "from the use of this software.");
 }
 
 static void spin_changed (GtkSpinButton * spin, void * data)
@@ -118,13 +97,28 @@ static void mixer_configure (void)
     gtk_widget_show_all (config_win);
 }
 
+static const char mixer_about[] =
+ "Channel Mixer Plugin for Audacious\n"
+ "Copyright 2011 John Lindgren\n\n"
+ "Redistribution and use in source and binary forms, with or without "
+ "modification, are permitted provided that the following conditions are "
+ "met:\n\n"
+ "1. Redistributions of source code must retain the above copyright "
+ "notice, this list of conditions, and the following disclaimer.\n\n"
+ "2. Redistributions in binary form must reproduce the above copyright "
+ "notice, this list of conditions, and the following disclaimer in the "
+ "documentation provided with the distribution.\n\n"
+ "This software is provided \"as is\" and without any warranty, express or "
+ "implied. In no event shall the authors be liable for any damages arising "
+ "from the use of this software.";
+
 AUD_EFFECT_PLUGIN
 (
     .name = N_("Channel Mixer"),
     .domain = PACKAGE,
+    .about_text = mixer_about,
     .init = mixer_init,
     .cleanup = mixer_cleanup,
-    .about = mixer_about,
     .configure = mixer_configure,
     .start = mixer_start,
     .process = mixer_process,
