5849bcbabbd2b6f8714bd66b2d96a8c8936f367c
John Lindgren <john.lindgren@aol.com>
2012-06-02 13:53:07 -0400

    gio: Use about_text.

--- src/gio/gio.c
+++ src/gio/gio.c
@@ -39,7 +39,6 @@ typedef struct {
 
 /* in gtk.c */
 void gio_error (const char * format, ...);
-void gio_about (void);
 
 #define CHECK_ERROR(op, name) do { \
     if (error) { \
@@ -296,6 +295,21 @@ FAILED:
     return -1;
 }
 
+static const char gio_about[] =
+ "GIO Plugin for Audacious\n"
+ "Copyright 2009-2012 John Lindgren\n\n"
+ "Redistribution and use in source and binary forms, with or without "
+ "modification, are permitted provided that the following conditions are "
+ "met:\n\n"
+ "1. Redistributions of source code must retain the above copyright "
+ "notice, this list of conditions, and the following disclaimer.\n\n"
+ "2. Redistributions in binary form must reproduce the above copyright "
+ "notice, this list of conditions, and the following disclaimer in the "
+ "documentation provided with the distribution.\n\n"
+ "This software is provided \"as is\" and without any warranty, express or "
+ "implied. In no event shall the authors be liable for any damages arising "
+ "from the use of this software.";
+
 static const char * const gio_schemes[] = {"ftp", "sftp", "smb", 0};
 
 static VFSConstructor constructor = {
@@ -317,7 +331,7 @@ AUD_TRANSPORT_PLUGIN
 (
  .name = N_("GIO Plugin"),
  .domain = PACKAGE,
- .about = gio_about,
+ .about_text = gio_about,
  .schemes = gio_schemes,
  .vtable = & constructor
 )
