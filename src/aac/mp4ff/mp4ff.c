7e6859d3654344a9dfb4406988c508556426a73a
John Lindgren <john.lindgren@aol.com>
2012-06-03 19:23:32 -0400

    aac[mp4ff]: Check for errors while parsing MP4 so as to avoid infinite looping. Closes: #116.

--- src/aac/mp4ff/mp4ff.c
+++ src/aac/mp4ff/mp4ff.c
@@ -1,19 +1,19 @@
 /*
 ** FAAD2 - Freeware Advanced Audio (AAC) Decoder including SBR decoding
 ** Copyright (C) 2003-2005 M. Bakker, Nero AG, http://www.nero.com
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
 ** Any non-GPL usage of this software or parts of this software is strictly
@@ -208,14 +208,19 @@ int32_t parse_atoms(mp4ff_t *f,int meta_only)
         /* parse subatoms */
 		if (meta_only && !need_parse_when_meta_only(atom_type))
 		{
-			mp4ff_set_position(f, mp4ff_position(f)+size-header_size);
-		} else if (atom_type < SUBATOMIC)
-        {
-            parse_sub_atoms(f, size-header_size,meta_only);
-        } else {
-            /* skip this atom */
-            mp4ff_set_position(f, mp4ff_position(f)+size-header_size);
-        }
+			if(mp4ff_set_position(f, mp4ff_position(f) + size - header_size) != 0)
+				break;
+		}
+		else if (atom_type < SUBATOMIC)
+		{
+			parse_sub_atoms(f, size-header_size,meta_only);
+		}
+		else
+		{
+			/* skip this atom */
+			if(mp4ff_set_position(f, mp4ff_position(f) + size - header_size) != 0)
+				break;
+		}
     }
 
     return 0;
