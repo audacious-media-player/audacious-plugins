5226a26efaafe350a2856042f061051742e478cf
John Lindgren <john.lindgren@aol.com>
2014-01-25 17:31:23 -0500

    delete-files: Stop playback first or deleting fails on Windows.

--- src/delete-files/delete-files.c
+++ src/delete-files/delete-files.c
@@ -63,15 +63,24 @@ static void really_delete (const char * filename)
 
 static void confirm_delete (void)
 {
+    Index * files = index_new ();
+
     int playlist = aud_playlist_get_active ();
     int entry_count = aud_playlist_entry_count (playlist);
 
     for (int i = 0; i < entry_count; i ++)
     {
-        if (! aud_playlist_entry_get_selected (playlist, i))
-            continue;
+        if (aud_playlist_entry_get_selected (playlist, i))
+            index_insert (files, -1, aud_playlist_entry_get_filename (playlist, i));
+    }
 
-        char * uri = aud_playlist_entry_get_filename (playlist, i);
+    aud_playlist_delete_selected (playlist);
+
+    int file_count = index_count (files);
+
+    for (int i = 0; i < file_count; i ++)
+    {
+        char * uri = index_get (files, i);
         char * filename = uri_to_filename (uri);
 
         if (filename)
@@ -88,11 +97,9 @@ static void confirm_delete (void)
             SPRINTF (error, _("Error deleting %s: not a local file."), uri);
             aud_interface_show_error (error);
         }
-
-        str_unref (uri);
     }
 
-    aud_playlist_delete_selected (playlist);
+    index_free_full (files, (IndexFreeFunc) str_unref);
 }
 
 static void start_delete (void)
