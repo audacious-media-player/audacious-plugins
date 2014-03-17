a299ce8660b0a5ec2f85c7d29ce95e442f170f7b
Thomas Lange <thomas-lange2@gmx.de>
2014-03-18 00:53:28 +0100

    skins: Use correct icon for cancel button.

--- src/skins/preset-list.c
+++ src/skins/preset-list.c
@@ -308,7 +308,7 @@ static GtkWidget * equalizerwin_create_list_window (Index * preset_list,
     gtk_box_set_spacing(GTK_BOX(bbox), 5);
     gtk_box_pack_start(GTK_BOX(vbox), bbox, FALSE, FALSE, 0);
 
-    button_cancel = audgui_button_new (_("Cancel"), "window-close", NULL, NULL);
+    button_cancel = audgui_button_new (_("Cancel"), "process-stop", NULL, NULL);
     g_signal_connect_swapped (button_cancel, "clicked", (GCallback)
      gtk_widget_destroy, * window);
     gtk_box_pack_start(GTK_BOX(bbox), button_cancel, TRUE, TRUE, 0);
