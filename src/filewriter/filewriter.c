955ebd8c0507b39e57055520a3f9e76eda3233fd
John Lindgren <john.lindgren@aol.com>
2014-05-02 19:38:51 -0400

    filewriter: Don't modify pooled string.

--- src/filewriter/filewriter.c
+++ src/filewriter/filewriter.c
@@ -203,13 +203,13 @@ static gint file_open(gint fmt, gint rate, gint nch)
     if (filenamefromtags)
     {
         gchar * utf8 = aud_playlist_entry_get_title (playlist, pos, FALSE);
-        str_replace_char (utf8, '/', ' ');
 
         gchar buf[3 * strlen (utf8) + 1];
         str_encode_percent (utf8, -1, buf);
-        str_unref (utf8);
-
+        str_replace_char (buf, '/', '-');
         filename = g_strdup (buf);
+
+        str_unref (utf8);
     }
     else
     {
