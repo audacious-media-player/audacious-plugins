1c7db8aaf88ec66a76f4d2151dc403cf909d6693
John Lindgren <john.lindgren@aol.com>
2014-07-12 11:05:13 -0400

    cairo-spectrum: Fix rounding error.  Found by Coverity.

--- src/cairo-spectrum/cairo-spectrum.cc
+++ src/cairo-spectrum/cairo-spectrum.cc
@@ -212,8 +212,6 @@ static void draw_grid (GtkWidget * area, cairo_t * cr)
 
 static void draw_visualizer (GtkWidget *widget, cairo_t *cr)
 {
-    float base_s = (height / 40);
-
     for (int i = 0; i < bands; i++)
     {
         int x = ((width / bands) * i) + 2;
@@ -221,7 +219,8 @@ static void draw_visualizer (GtkWidget *widget, cairo_t *cr)
 
         get_color (widget, i, & r, & g, & b);
         cairo_set_source_rgb (cr, r, g, b);
-        cairo_rectangle (cr, x + 1, height - (bars[i] * base_s), (width / bands) - 1, (bars[i] * base_s));
+        cairo_rectangle (cr, x + 1, height - (bars[i] * height / 40),
+         (width / bands) - 1, (bars[i] * height / 40));
         cairo_fill (cr);
     }
 }
