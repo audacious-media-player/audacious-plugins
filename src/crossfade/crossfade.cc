6d26f6092a8372dab215adc6a02e975cf3f66ff3
John Lindgren <john.lindgren@aol.com>
2014-10-04 20:11:39 -0400

    crossfade: Rewrite to implement crossfade on manual song change.

--- src/crossfade/crossfade.cc
+++ src/crossfade/crossfade.cc
@@ -1,6 +1,6 @@
 /*
  * Crossfade Plugin for Audacious
- * Copyright 2010-2012 John Lindgren
+ * Copyright 2010-2014 John Lindgren
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -17,9 +17,6 @@
  * the use of this software.
  */
 
-#include <stdlib.h>
-#include <string.h>
-
 #include <libaudcore/i18n.h>
 #include <libaudcore/interface.h>
 #include <libaudcore/plugin.h>
@@ -29,25 +26,38 @@
 enum
 {
     STATE_OFF,
-    STATE_PREBUFFER,
+    STATE_FADEIN,
     STATE_RUNNING,
-    STATE_BETWEEN,
-    STATE_STOPPING,
+    STATE_FINISHED,
+    STATE_FLUSHED
 };
 
 static const char * const crossfade_defaults[] = {
- "length", "3",
- nullptr};
+    "automatic", "TRUE",
+    "length", "5",
+    "manual", "TRUE",
+    "manual_length", "0.2",
+    nullptr
+};
 
 static const char crossfade_about[] =
  N_("Crossfade Plugin for Audacious\n"
-    "Copyright 2010-2012 John Lindgren");
+    "Copyright 2010-2014 John Lindgren");
 
 static const PreferencesWidget crossfade_widgets[] = {
     WidgetLabel (N_("<b>Crossfade</b>")),
+    WidgetCheck (N_("On automatic song change"),
+        WidgetBool ("crossfade", "automatic")),
     WidgetSpin (N_("Overlap:"),
-        WidgetInt ("crossfade", "length"),
-        {1, 10, 1, N_("seconds")})
+        WidgetFloat ("crossfade", "length"),
+        {1, 15, 0.5, N_("seconds")},
+        WIDGET_CHILD),
+    WidgetCheck (N_("On seek or manual song change"),
+        WidgetBool ("crossfade", "manual")),
+    WidgetSpin (N_("Overlap:"),
+        WidgetFloat ("crossfade", "manual_length"),
+        {0.1, 3.0, 0.1, N_("seconds")},
+        WIDGET_CHILD)
 };
 
 static const PluginPreferences crossfade_prefs = {{crossfade_widgets}};
@@ -70,8 +80,8 @@ public:
 
     void start (int & channels, int & rate);
     Index<float> & process (Index<float> & data);
-    void flush ();
-    Index<float> & finish (Index<float> & data);
+    bool flush (bool force);
+    Index<float> & finish (Index<float> & data, bool end_of_playlist);
     int adjust_delay (int delay);
 };
 
@@ -80,7 +90,7 @@ EXPORT Crossfade aud_plugin_instance;
 static char state = STATE_OFF;
 static int current_channels = 0, current_rate = 0;
 static Index<float> buffer, output;
-static int prebuffer_filled = 0;
+static int fadein_point;
 
 static void reset ()
 {
@@ -89,7 +99,6 @@ static void reset ()
     current_rate = 0;
     buffer.clear ();
     output.clear ();
-    prebuffer_filled = 0;
 }
 
 bool Crossfade::init ()
@@ -103,38 +112,11 @@ void Crossfade::cleanup ()
     reset ();
 }
 
-void Crossfade::start (int & channels, int & rate)
-{
-    if (state != STATE_BETWEEN)
-        reset ();
-    else if (channels != current_channels)
-    {
-        aud_ui_show_error (_("Crossfading failed because the songs had "
-         "a different number of channels.  You can use the Channel Mixer to "
-         "convert the songs to the same number of channels."));
-        reset ();
-    }
-    else if (rate != current_rate)
-    {
-        aud_ui_show_error (_("Crossfading failed because the songs had "
-         "different sample rates.  You can use the Sample Rate Converter to "
-         "convert the songs to the same sample rate."));
-        reset ();
-    }
-
-    state = STATE_PREBUFFER;
-    current_channels = channels;
-    current_rate = rate;
-    prebuffer_filled = 0;
-}
-
 static void do_ramp (float * data, int length, float a, float b)
 {
-    int count;
-
-    for (count = 0; count < length; count ++)
+    for (int i = 0; i < length; i ++)
     {
-        * data = (* data) * (a * (length - count) + b * count) / length;
+        * data = (* data) * (a * (length - i) + b * i) / length;
         data ++;
     }
 }
@@ -145,99 +127,173 @@ static void mix (float * data, float * add, int length)
         (* data ++) += (* add ++);
 }
 
-static void add_data (float * data, int length)
+static int buffer_needed_for_state ()
 {
-    if (state == STATE_PREBUFFER)
-    {
-        int full = current_channels * current_rate * aud_get_int ("crossfade", "length");
+    double overlap = 0;
 
-        if (prebuffer_filled < full)
-        {
-            int copy = aud::min (length, full - prebuffer_filled);
-            float a = (float) prebuffer_filled / full;
-            float b = (float) (prebuffer_filled + copy) / full;
+    if (state != STATE_FLUSHED && aud_get_bool ("crossfade", "automatic"))
+        overlap = aud_get_double ("crossfade", "length");
 
-            if (prebuffer_filled + copy > buffer.len ())
-                buffer.insert (-1, prebuffer_filled + copy - buffer.len ());
+    if (state != STATE_FINISHED && aud_get_bool ("crossfade", "manual"))
+        overlap = aud::max (overlap, aud_get_double ("crossfade", "manual_length"));
 
-            do_ramp (data, copy, a, b);
-            mix (& buffer[prebuffer_filled], data, copy);
-            prebuffer_filled += copy;
-            data += copy;
-            length -= copy;
-        }
-
-        if (prebuffer_filled < full)
-            return;
-
-        if (prebuffer_filled < buffer.len ())
-        {
-            int copy = aud::min (length, buffer.len () - prebuffer_filled);
-
-            mix (& buffer[prebuffer_filled], data, copy);
-            prebuffer_filled += copy;
-            data += copy;
-            length -= copy;
-        }
-
-        if (prebuffer_filled < buffer.len ())
-            return;
-
-        state = STATE_RUNNING;
-    }
-
-    if (state != STATE_RUNNING)
-        return;
-
-    buffer.insert (data, -1, length);
+    return current_channels * (int) (current_rate * overlap);
 }
 
-static void output_data ()
+static void output_data_as_ready (int buffer_needed, bool exact)
 {
-    int full = current_channels * current_rate * aud_get_int ("crossfade", "length");
-    int copy = buffer.len () - full;
+    int copy = buffer.len () - buffer_needed;
 
-    output.resize (0);
+    /* if allowed, wait until we have at least 1/2 second ready to output */
+    if (exact ? (copy > 0) : (copy >= current_channels * (current_rate / 2)))
+        output.move_from (buffer, 0, -1, copy, true, true);
+}
 
-    /* only return if we have at least 1/2 second -- this reduces memmove's */
-    if (state != STATE_RUNNING || copy < current_channels * (current_rate / 2))
-        return;
+void Crossfade::start (int & channels, int & rate)
+{
+    if (state != STATE_OFF)
+    {
+        if (channels != current_channels)
+        {
+            aud_ui_show_error (_("Crossfading failed because the songs had "
+             "a different number of channels.  You can use the Channel Mixer to "
+             "convert the songs to the same number of channels."));
+            state = STATE_OFF;
+        }
+        else if (rate != current_rate)
+        {
+            aud_ui_show_error (_("Crossfading failed because the songs had "
+             "different sample rates.  You can use the Sample Rate Converter to "
+             "convert the songs to the same sample rate."));
+            state = STATE_OFF;
+        }
+    }
 
-    output.move_from (buffer, 0, 0, copy, true, true);
+    if (state == STATE_OFF)
+    {
+        reset ();
+
+        current_channels = channels;
+        current_rate = rate;
+
+        if (aud_get_bool ("crossfade", "manual"))
+        {
+            state = STATE_FLUSHED;
+            buffer.insert (0, buffer_needed_for_state ());
+        }
+        else
+            state = STATE_RUNNING;
+    }
+}
+
+static void run_fadeout ()
+{
+    do_ramp (buffer.begin (), buffer.len (), 1.0, 0.0);
+
+    state = STATE_FADEIN;
+    fadein_point = 0;
+}
+
+static void run_fadein (Index<float> & data)
+{
+    int length = buffer.len ();
+
+    if (fadein_point < length)
+    {
+        int copy = aud::min (data.len (), length - fadein_point);
+        float a = (float) fadein_point / length;
+        float b = (float) (fadein_point + copy) / length;
+
+        do_ramp (data.begin (), copy, a, b);
+        mix (& buffer[fadein_point], data.begin (), copy);
+        data.remove (0, copy);
+
+        fadein_point += copy;
+    }
+
+    if (fadein_point == length)
+        state = STATE_RUNNING;
 }
 
 Index<float> & Crossfade::process (Index<float> & data)
 {
-    add_data (data.begin (), data.len ());
-    output_data ();
+    if (state == STATE_OFF)
+        return data;
+
+    output.resize (0);
+
+    if (state == STATE_FINISHED || state == STATE_FLUSHED)
+        run_fadeout ();
+
+    if (state == STATE_FADEIN)
+        run_fadein (data);
+
+    if (state == STATE_RUNNING)
+    {
+        buffer.insert (data.begin (), -1, data.len ());
+        output_data_as_ready (buffer_needed_for_state (), false);
+    }
+
     return output;
 }
 
-void Crossfade::flush ()
+bool Crossfade::flush (bool force)
 {
-    if (state == STATE_PREBUFFER || state == STATE_RUNNING)
+    if (state == STATE_OFF)
+        return true;
+
+    if (! force && aud_get_bool ("crossfade", "manual"))
     {
-        state = STATE_RUNNING;
-        buffer.resize (0);
+        state = STATE_FLUSHED;
+        int buffer_needed = buffer_needed_for_state ();
+        if (buffer.len () > buffer_needed)
+            buffer.remove (buffer_needed, -1);
+
+        return false;
     }
+
+    state = STATE_RUNNING;
+    buffer.resize (0);
+
+    return true;
 }
 
-Index<float> & Crossfade::finish (Index<float> & data)
+Index<float> & Crossfade::finish (Index<float> & data, bool end_of_playlist)
 {
-    if (state == STATE_BETWEEN) /* second call, end of last song */
+    if (state == STATE_OFF)
+        return data;
+
+    output.resize (0);
+
+    if (state == STATE_FADEIN)
+        run_fadein (data);
+
+    if (state == STATE_RUNNING || state == STATE_FINISHED || state == STATE_FLUSHED)
     {
-        output = std::move (buffer);
-        state = STATE_OFF;
-        return output;
+        buffer.insert (data.begin (), -1, data.len ());
+        output_data_as_ready (buffer_needed_for_state (), state != STATE_RUNNING);
     }
 
-    add_data (data.begin (), data.len ());
-    output_data ();
+    if (state == STATE_FADEIN || state == STATE_RUNNING)
+    {
+        if (aud_get_bool ("crossfade", "automatic"))
+        {
+            state = STATE_FINISHED;
+            output_data_as_ready (buffer_needed_for_state (), true);
+        }
+        else
+        {
+            state = STATE_OFF;
+            output_data_as_ready (0, true);
+        }
+    }
 
-    if (state == STATE_PREBUFFER || state == STATE_RUNNING)
+    if (end_of_playlist && (state == STATE_FINISHED || state == STATE_FLUSHED))
     {
         do_ramp (buffer.begin (), buffer.len (), 1.0, 0.0);
-        state = STATE_BETWEEN;
+
+        state = STATE_OFF;
+        output_data_as_ready (0, true);
     }
 
     return output;
