86afef8c5b471c32de0b02a2fd5a806b911f75af
Thomas Lange <thomas-lange2@gmx.de>
2015-08-15 14:40:23 +0200

    lyricwiki: Use Audacious' coding style.

--- src/lyricwiki/lyricwiki.cc
+++ src/lyricwiki/lyricwiki.cc
@@ -62,15 +62,15 @@ static LyricsState state;
  * Suppress libxml warnings, because lyricwiki does not generate anything near
  * valid HTML.
  */
-static void libxml_error_handler(void *ctx, const char *msg, ...)
+static void libxml_error_handler (void * ctx, const char * msg, ...)
 {
 }
 
 /* g_free() returned text */
-static char *scrape_lyrics_from_lyricwiki_edit_page(const char *buf, int64_t len)
+static char * scrape_lyrics_from_lyricwiki_edit_page (const char * buf, int64_t len)
 {
     xmlDocPtr doc;
-    char *ret = nullptr;
+    char * ret = nullptr;
 
     /*
      * temporarily set our error-handling functor to our suppression function,
@@ -81,70 +81,70 @@ static char *scrape_lyrics_from_lyricwiki_edit_page(const char *buf, int64_t len
      * the previous error functor, so we just have to set it back to default after
      * parsing and hope for the best.
      */
-    xmlSetGenericErrorFunc(nullptr, libxml_error_handler);
-    doc = htmlReadMemory(buf, (int) len, nullptr, "utf-8", (HTML_PARSE_RECOVER | HTML_PARSE_NONET));
-    xmlSetGenericErrorFunc(nullptr, nullptr);
+    xmlSetGenericErrorFunc (nullptr, libxml_error_handler);
+    doc = htmlReadMemory (buf, (int) len, nullptr, "utf-8", (HTML_PARSE_RECOVER | HTML_PARSE_NONET));
+    xmlSetGenericErrorFunc (nullptr, nullptr);
 
-    if (doc != nullptr)
+    if (doc)
     {
         xmlXPathContextPtr xpath_ctx = nullptr;
         xmlXPathObjectPtr xpath_obj = nullptr;
         xmlNodePtr node = nullptr;
 
-        xpath_ctx = xmlXPathNewContext(doc);
-        if (xpath_ctx == nullptr)
+        xpath_ctx = xmlXPathNewContext (doc);
+        if (! xpath_ctx)
             goto give_up;
 
-        xpath_obj = xmlXPathEvalExpression((xmlChar *) "//*[@id=\"wpTextbox1\"]", xpath_ctx);
-        if (xpath_obj == nullptr)
+        xpath_obj = xmlXPathEvalExpression ((xmlChar *) "//*[@id=\"wpTextbox1\"]", xpath_ctx);
+        if (! xpath_obj)
             goto give_up;
 
-        if (!xpath_obj->nodesetval->nodeMax)
+        if (! xpath_obj->nodesetval->nodeMax)
             goto give_up;
 
         node = xpath_obj->nodesetval->nodeTab[0];
 give_up:
-        if (xpath_obj != nullptr)
-            xmlXPathFreeObject(xpath_obj);
+        if (xpath_obj)
+            xmlXPathFreeObject (xpath_obj);
 
-        if (xpath_ctx != nullptr)
-            xmlXPathFreeContext(xpath_ctx);
+        if (xpath_ctx)
+            xmlXPathFreeContext (xpath_ctx);
 
-        if (node != nullptr)
+        if (node)
         {
-            xmlChar *lyric = xmlNodeGetContent(node);
+            xmlChar * lyric = xmlNodeGetContent (node);
 
-            if (lyric != nullptr)
+            if (lyric)
             {
-                GMatchInfo *match_info;
-                GRegex *reg;
+                GMatchInfo * match_info;
+                GRegex * reg;
 
                 reg = g_regex_new
                  ("<(lyrics?)>[[:space:]]*(.*?)[[:space:]]*</\\1>",
                  (GRegexCompileFlags) (G_REGEX_MULTILINE | G_REGEX_DOTALL),
                  (GRegexMatchFlags) 0, nullptr);
-                g_regex_match(reg, (char *) lyric, G_REGEX_MATCH_NEWLINE_ANY, &match_info);
+                g_regex_match (reg, (char *) lyric, G_REGEX_MATCH_NEWLINE_ANY, & match_info);
 
-                ret = g_match_info_fetch(match_info, 2);
-                if (!g_utf8_collate(ret, "<!-- PUT LYRICS HERE (and delete this entire line) -->"))
+                ret = g_match_info_fetch (match_info, 2);
+                if (! g_utf8_collate (ret, "<!-- PUT LYRICS HERE (and delete this entire line) -->"))
                 {
-                    g_free(ret);
-                    ret = g_strdup(_("No lyrics available"));
+                    g_free (ret);
+                    ret = g_strdup (_("No lyrics available"));
                 }
 
-                g_regex_unref(reg);
+                g_regex_unref (reg);
             }
 
-            xmlFree(lyric);
+            xmlFree (lyric);
         }
 
-        xmlFreeDoc(doc);
+        xmlFreeDoc (doc);
     }
 
     return ret;
 }
 
-static String scrape_uri_from_lyricwiki_search_result(const char *buf, int64_t len)
+static String scrape_uri_from_lyricwiki_search_result (const char * buf, int64_t len)
 {
     xmlDocPtr doc;
     String uri;
@@ -153,13 +153,13 @@ static String scrape_uri_from_lyricwiki_search_result(const char *buf, int64_t l
      * workaround buggy lyricwiki search output where it cuts the lyrics
      * halfway through the UTF-8 symbol resulting in invalid XML.
      */
-    GRegex *reg;
+    GRegex * reg;
 
     reg = g_regex_new ("<(lyrics?)>.*</\\1>", (GRegexCompileFlags)
      (G_REGEX_MULTILINE | G_REGEX_DOTALL | G_REGEX_UNGREEDY),
      (GRegexMatchFlags) 0, nullptr);
-    char *newbuf = g_regex_replace_literal(reg, buf, len, 0, "", G_REGEX_MATCH_NEWLINE_ANY, nullptr);
-    g_regex_unref(reg);
+    char * newbuf = g_regex_replace_literal (reg, buf, len, 0, "", G_REGEX_MATCH_NEWLINE_ANY, nullptr);
+    g_regex_unref (reg);
 
     /*
      * temporarily set our error-handling functor to our suppression function,
@@ -170,9 +170,9 @@ static String scrape_uri_from_lyricwiki_search_result(const char *buf, int64_t l
      * the previous error functor, so we just have to set it back to default after
      * parsing and hope for the best.
      */
-    xmlSetGenericErrorFunc(nullptr, libxml_error_handler);
-    doc = xmlParseMemory(newbuf, strlen(newbuf));
-    xmlSetGenericErrorFunc(nullptr, nullptr);
+    xmlSetGenericErrorFunc (nullptr, libxml_error_handler);
+    doc = xmlParseMemory (newbuf, strlen (newbuf));
+    xmlSetGenericErrorFunc (nullptr, nullptr);
 
     if (doc != nullptr)
     {
@@ -184,72 +184,72 @@ static String scrape_uri_from_lyricwiki_search_result(const char *buf, int64_t l
         {
             if (xmlStrEqual(cur->name, (xmlChar *) "url"))
             {
-                xmlChar *lyric;
-                char *basename;
+                xmlChar * lyric;
+                char * basename;
 
-                lyric = xmlNodeGetContent(cur);
-                basename = g_path_get_basename((char *) lyric);
+                lyric = xmlNodeGetContent (cur);
+                basename = g_path_get_basename ((char *) lyric);
 
                 uri = String (str_printf ("http://lyrics.wikia.com/index.php?"
                  "action=edit&title=%s", basename));
 
-                g_free(basename);
-                xmlFree(lyric);
+                g_free (basename);
+                xmlFree (lyric);
             }
         }
 
-        xmlFreeDoc(doc);
+        xmlFreeDoc (doc);
     }
 
-    g_free(newbuf);
+    g_free (newbuf);
 
     return uri;
 }
 
-static void update_lyrics_window(const char *title, const char *artist,
- const char *lyrics, bool edit_enabled);
+static void update_lyrics_window (const char * title, const char * artist,
+ const char * lyrics, bool edit_enabled);
 
-static void get_lyrics_step_3(const char *uri, const Index<char> &buf, void*)
+static void get_lyrics_step_3 (const char * uri, const Index<char> & buf, void *)
 {
-    if (!state.uri || strcmp(state.uri, uri))
+    if (! state.uri || strcmp (state.uri, uri))
         return;
 
-    if (!buf.len())
+    if (! buf.len ())
     {
         update_lyrics_window (_("Error"), nullptr,
          str_printf (_("Unable to fetch %s"), uri), true);
         return;
     }
 
-    char *lyrics = scrape_lyrics_from_lyricwiki_edit_page(buf.begin(), buf.len());
+    char * lyrics = scrape_lyrics_from_lyricwiki_edit_page (buf.begin (), buf.len ());
 
-    if (!lyrics)
+    if (! lyrics)
     {
         update_lyrics_window (_("Error"), nullptr,
          str_printf (_("Unable to parse %s"), uri), true);
         return;
     }
 
-    update_lyrics_window(state.title, state.artist, lyrics, true);
+    update_lyrics_window (state.title, state.artist, lyrics, true);
 
-    g_free(lyrics);
+    g_free (lyrics);
 }
 
-static void get_lyrics_step_2(const char *uri1, const Index<char> &buf, void*)
+static void get_lyrics_step_2 (const char * uri1, const Index<char> & buf, void *)
 {
-    if (!state.uri || strcmp(state.uri, uri1))
+    if (! state.uri || strcmp(state.uri, uri1))
         return;
 
-    if (!buf.len())
+    if (! buf.len ())
     {
         update_lyrics_window (_("Error"), nullptr,
          str_printf (_("Unable to fetch %s"), uri1), false);
         return;
     }
 
-    String uri = scrape_uri_from_lyricwiki_search_result(buf.begin(), buf.len());
+    String uri = scrape_uri_from_lyricwiki_search_result (buf.begin (), buf.len ());
 
-    if (!uri)
+    if (! uri)
     {
         update_lyrics_window (_("Error"), nullptr,
          str_printf (_("Unable to parse %s"), uri1), false);
@@ -258,15 +258,15 @@ static void get_lyrics_step_2(const char *uri1, const Index<char> &buf, void*)
 
     state.uri = uri;
 
-    update_lyrics_window(state.title, state.artist, _("Looking for lyrics ..."), true);
-    vfs_async_file_get_contents(uri, get_lyrics_step_3, nullptr);
+    update_lyrics_window (state.title, state.artist, _("Looking for lyrics ..."), true);
+    vfs_async_file_get_contents (uri, get_lyrics_step_3, nullptr);
 }
 
-static void get_lyrics_step_1(void)
+static void get_lyrics_step_1 ()
 {
-    if(!state.artist || !state.title)
+    if (! state.artist || ! state.title)
     {
-        update_lyrics_window(_("Error"), nullptr, _("Missing song metadata"), false);
+        update_lyrics_window (_("Error"), nullptr, _("Missing song metadata"), false);
         return;
     }
 
@@ -277,13 +277,13 @@ static void get_lyrics_step_1(void)
      "action=lyrics&artist=%s&song=%s&fmt=xml", (const char *) artist_buf,
      (const char *) title_buf));
 
-    update_lyrics_window(state.title, state.artist, _("Connecting to lyrics.wikia.com ..."), false);
-    vfs_async_file_get_contents(state.uri, get_lyrics_step_2, nullptr);
+    update_lyrics_window (state.title, state.artist, _("Connecting to lyrics.wikia.com ..."), false);
+    vfs_async_file_get_contents (state.uri, get_lyrics_step_2, nullptr);
 }
 
-static GtkTextView *textview;
-static GtkWidget *edit_button;
-static GtkTextBuffer *textbuffer;
+static GtkTextView * textview;
+static GtkWidget * edit_button;
+static GtkTextBuffer * textbuffer;
 
 static void launch_edit_page ()
 {
@@ -291,30 +291,29 @@ static void launch_edit_page ()
         gtk_show_uri (nullptr, state.uri, GDK_CURRENT_TIME, nullptr);
 }
 
-static GtkWidget *build_widget(void)
+static GtkWidget * build_widget ()
 {
-    textview = (GtkTextView *) gtk_text_view_new();
-    gtk_text_view_set_editable(textview, false);
-    gtk_text_view_set_cursor_visible(textview, false);
-    gtk_text_view_set_left_margin(textview, 4);
-    gtk_text_view_set_right_margin(textview, 4);
-    gtk_text_view_set_wrap_mode(textview, GTK_WRAP_WORD);
-    textbuffer = gtk_text_view_get_buffer(textview);
+    textview = (GtkTextView *) gtk_text_view_new ();
+    gtk_text_view_set_editable (textview, false);
+    gtk_text_view_set_cursor_visible (textview, false);
+    gtk_text_view_set_left_margin (textview, 4);
+    gtk_text_view_set_right_margin (textview, 4);
+    gtk_text_view_set_wrap_mode (textview, GTK_WRAP_WORD);
+    textbuffer = gtk_text_view_get_buffer (textview);
 
-    GtkWidget *scrollview = gtk_scrolled_window_new(nullptr, nullptr);
-    gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrollview), GTK_SHADOW_IN);
-    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollview), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-    GtkWidget *vbox = gtk_vbox_new (false, 6);
+    GtkWidget * scrollview = gtk_scrolled_window_new (nullptr, nullptr);
+    gtk_scrolled_window_set_shadow_type ((GtkScrolledWindow *) scrollview, GTK_SHADOW_IN);
+    gtk_scrolled_window_set_policy ((GtkScrolledWindow *) scrollview, GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    GtkWidget * vbox = gtk_vbox_new (false, 6);
 
-    gtk_container_add(GTK_CONTAINER(scrollview), GTK_WIDGET(textview));
+    gtk_container_add ((GtkContainer *) scrollview, (GtkWidget *) textview);
+    gtk_box_pack_start ((GtkBox *) vbox, scrollview, true, true, 0);
 
-    gtk_box_pack_start(GTK_BOX(vbox), scrollview, true, true, 0);
+    gtk_widget_show_all (vbox);
 
-    gtk_widget_show_all(vbox);
-
-    gtk_text_buffer_create_tag(textbuffer, "weight_bold", "weight", PANGO_WEIGHT_BOLD, nullptr);
-    gtk_text_buffer_create_tag(textbuffer, "size_x_large", "scale", PANGO_SCALE_X_LARGE, nullptr);
-    gtk_text_buffer_create_tag(textbuffer, "style_italic", "style", PANGO_STYLE_ITALIC, nullptr);
+    gtk_text_buffer_create_tag (textbuffer, "weight_bold", "weight", PANGO_WEIGHT_BOLD, nullptr);
+    gtk_text_buffer_create_tag (textbuffer, "size_x_large", "scale", PANGO_SCALE_X_LARGE, nullptr);
+    gtk_text_buffer_create_tag (textbuffer, "style_italic", "style", PANGO_STYLE_ITALIC, nullptr);
 
     GtkWidget * hbox = gtk_hbox_new (false, 6);
     gtk_box_pack_start ((GtkBox *) vbox, hbox, false, false, 0);
@@ -328,50 +327,50 @@ static GtkWidget *build_widget(void)
     return vbox;
 }
 
-static void update_lyrics_window(const char *title, const char *artist,
- const char *lyrics, bool edit_enabled)
+static void update_lyrics_window (const char * title, const char * artist,
+ const char * lyrics, bool edit_enabled)
 {
     GtkTextIter iter;
 
-    if (textbuffer == nullptr)
+    if (! textbuffer)
         return;
 
-    gtk_text_buffer_set_text(textbuffer, "", -1);
+    gtk_text_buffer_set_text (textbuffer, "", -1);
 
-    gtk_text_buffer_get_start_iter(textbuffer, &iter);
+    gtk_text_buffer_get_start_iter (textbuffer, & iter);
 
-    gtk_text_buffer_insert_with_tags_by_name(textbuffer, &iter, title, -1,
+    gtk_text_buffer_insert_with_tags_by_name (textbuffer, & iter, title, -1,
      "weight_bold", "size_x_large", nullptr);
 
-    if (artist != nullptr)
+    if (artist)
     {
-        gtk_text_buffer_insert(textbuffer, &iter, "\n", -1);
-        gtk_text_buffer_insert_with_tags_by_name(textbuffer, &iter, artist, -1,
+        gtk_text_buffer_insert (textbuffer, & iter, "\n", -1);
+        gtk_text_buffer_insert_with_tags_by_name (textbuffer, & iter, artist, -1,
          "style_italic", nullptr);
     }
 
-    gtk_text_buffer_insert(textbuffer, &iter, "\n\n", -1);
-    gtk_text_buffer_insert(textbuffer, &iter, lyrics, -1);
+    gtk_text_buffer_insert (textbuffer, & iter, "\n\n", -1);
+    gtk_text_buffer_insert (textbuffer, & iter, lyrics, -1);
 
-    gtk_text_buffer_get_start_iter(textbuffer, &iter);
-    gtk_text_view_scroll_to_iter(textview, &iter, 0, true, 0, 0);
+    gtk_text_buffer_get_start_iter (textbuffer, & iter);
+    gtk_text_view_scroll_to_iter (textview, & iter, 0, true, 0, 0);
 
     gtk_widget_set_sensitive (edit_button, edit_enabled);
 }
 
-static void lyricwiki_playback_began(void)
+static void lyricwiki_playback_began ()
 {
     /* FIXME: cancel previous VFS requests (not possible with current API) */
 
-    state.filename = aud_drct_get_filename();
+    state.filename = aud_drct_get_filename ();
 
-    Tuple tuple = aud_drct_get_tuple();
-    state.title = tuple.get_str(Tuple::Title);
-    state.artist = tuple.get_str(Tuple::Artist);
+    Tuple tuple = aud_drct_get_tuple ();
+    state.title = tuple.get_str (Tuple::Title);
+    state.artist = tuple.get_str (Tuple::Artist);
 
     state.uri = String ();
 
-    get_lyrics_step_1();
+    get_lyrics_step_1 ();
 }
 
 static void destroy_cb ()
@@ -385,8 +384,8 @@ static void destroy_cb ()
     hook_dissociate ("playback ready", (HookFunction) lyricwiki_playback_began);
 
     textview = nullptr;
-    edit_button = nullptr;
     textbuffer = nullptr;
+    edit_button = nullptr;
 }
 
 void * LyricWiki::get_gtk_widget ()
