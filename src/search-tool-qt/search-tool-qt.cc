18eae1b80655cce46e2fb5f5cbc3444c7516ce39
John Lindgren <john@jlindgren.net>
2018-01-22 21:42:59 -0500

    search-tool-qt: Remove Qt4 anachronism.

--- src/search-tool-qt/search-tool-qt.cc
+++ src/search-tool-qt/search-tool-qt.cc
@@ -175,32 +175,32 @@ protected:
     QStyle *appStyle = nullptr;
 };
 
-void HtmlDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+void HtmlDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option_, const QModelIndex &index) const
 {
-    QStyleOptionViewItemV4 optionV4 = option;
-    initStyleOption(&optionV4, index);
+    QStyleOptionViewItem option = option_;
+    initStyleOption(&option, index);
 
-    QStyle *style = optionV4.widget? optionV4.widget->style() : qApp->style();
+    QStyle *style = option.widget? option.widget->style() : qApp->style();
 
     QTextDocument doc;
-    doc.setHtml(optionV4.text);
-    doc.setTextWidth(optionV4.rect.width());
+    doc.setHtml(option.text);
+    doc.setTextWidth(option.rect.width());
     doc.setDocumentMargin(0);
-    doc.setDefaultFont(optionV4.font);
+    doc.setDefaultFont(option.font);
 
     /// Painting item without text
-    optionV4.text = QString();
-    style->drawControl(QStyle::CE_ItemViewItem, &optionV4, painter);
+    option.text = QString();
+    style->drawControl(QStyle::CE_ItemViewItem, &option, painter);
 
     QAbstractTextDocumentLayout::PaintContext ctx;
 
     // Highlighting text if item is selected
-    if (optionV4.state & QStyle::State_Selected)
-        ctx.palette.setColor(QPalette::Text, optionV4.palette.color(QPalette::Active, QPalette::HighlightedText));
+    if (option.state & QStyle::State_Selected)
+        ctx.palette.setColor(QPalette::Text, option.palette.color(QPalette::Active, QPalette::HighlightedText));
     else
-        ctx.palette.setColor(QPalette::Text, optionV4.palette.color(QPalette::Active, QPalette::Text));
+        ctx.palette.setColor(QPalette::Text, option.palette.color(QPalette::Active, QPalette::Text));
 
-    QRect textRect = style->subElementRect(QStyle::SE_ItemViewItemText, &optionV4);
+    QRect textRect = style->subElementRect(QStyle::SE_ItemViewItemText, &option);
     painter->save();
     painter->translate(textRect.topLeft());
     painter->setClipRect(textRect.translated(-textRect.topLeft()));
@@ -208,16 +208,16 @@ void HtmlDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, 
     painter->restore();
 }
 
-QSize HtmlDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
+QSize HtmlDelegate::sizeHint(const QStyleOptionViewItem &option_, const QModelIndex &index) const
 {
-    QStyleOptionViewItemV4 optionV4 = option;
-    initStyleOption(&optionV4, index);
+    QStyleOptionViewItem option = option_;
+    initStyleOption(&option, index);
 
     QTextDocument doc;
-    doc.setHtml(optionV4.text);
-    doc.setTextWidth(optionV4.rect.width());
+    doc.setHtml(option.text);
+    doc.setTextWidth(option.rect.width());
     doc.setDocumentMargin(0);
-    doc.setDefaultFont(optionV4.font);
+    doc.setDefaultFont(option.font);
     return QSize(doc.idealWidth(), doc.size().height());
 }
 
