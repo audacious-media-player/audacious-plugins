72eee5821f2f96d6b9f13434ff13563d1c118433
Domen Mori <domen.mory@gmail.com>
2018-10-15 20:46:00 +0200

    Codestyle and licence, songchange plugin for Win32

--- src/songchange/songchange_crossplatform.h
+++ src/songchange/songchange_crossplatform.h
@@ -1,6 +1,29 @@
-//
-// Created by domen on 15. 10. 2018.
-//
+/*
+ *  songchange_crossplatform.h
+ *  Audacious
+ *
+ *  Copyright (C) 2005-2018  Audacious team
+ *
+ *  XMMS - Cross-platform multimedia player
+ *  Copyright (C) 1998-2003  Peter Alm, Mikael Alm, Olle Hallnas,
+ *                           Thomas Nilsson and 4Front Technologies
+ *  Copyright (C) 1999-2003  Haavard Kvaalen
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; under version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses>.
+ *
+ *  The Audacious team does not consider modular code linking to
+ *  Audacious or using our public API to be a derived work.
+*/
 
 #ifndef AUDACIOUS1_SONGCHANGE_CROSSPLATFORM_H
 #define AUDACIOUS1_SONGCHANGE_CROSSPLATFORM_H
@@ -9,98 +32,32 @@
 
 #ifdef _WIN32
 
-#include <string>
 #include <windows.h>
+#include <glib.h>
 
-std::string utf16_to_utf8(const std::wstring &utf16)
-{
-    // Special case of empty input string
-    if (utf16.empty())
-    {
-        // Return empty string
-        return {};
-    }
-
-
-    // "Code page" value used with WideCharToMultiByte() for UTF-8 conversion
-    const UINT codePageUtf8 = CP_UTF8;
-
-    // Safely fails if an invalid UTF-16 character is encountered
-    const DWORD flags = WC_ERR_INVALID_CHARS;
-
-    // Get the length, in chars, of the resulting UTF-8 string
-    const int utf8Length = ::WideCharToMultiByte(
-            codePageUtf8,       // convert to UTF-8
-            flags,              // conversion flags
-            utf16.c_str(),  // source UTF-16 string
-            utf16.size(),  // length of source UTF-16 string, in WCHARs
-            nullptr,            // unused - no conversion required in this step
-            0,                  // request size of destination buffer, in chars
-            nullptr, nullptr) + 1;  // unused
-    if (utf8Length == 1)
-    {
-        // Conversion error
-        return {};
-    }
-
-    // Allocate destination buffer to store the resulting UTF-8 string
-    char* bytes = new char[utf8Length]{0};
-
-    // Do the conversion from UTF-16 to UTF-8
-    int result = ::WideCharToMultiByte(
-            codePageUtf8,       // convert to UTF-8
-            flags,              // conversion flags
-            utf16.c_str(),  // source UTF-16 string
-            utf16.size(),  // length of source UTF-16 string, in WCHARs
-            bytes,         // pointer to destination buffer
-            utf8Length,         // size of destination buffer, in chars
-            nullptr, nullptr);  // unused
-    if (result == 0)
-    {
-        return {};
-    }
-
-    auto returning = std::string(bytes);
-    delete [] bytes;
-    // Return resulting UTF-8 string
-    return returning;
-}
-
-std::wstring utf8_to_utf16(const std::string &utf8) {
-    int wchars_num = MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, NULL, 0);
-    auto *wstr = new wchar_t[wchars_num];
-    MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, wstr, wchars_num);
-    std::wstring result(wstr);
-    delete[] wstr;
-    return result;
-}
 
 static void signal_child() {
    // signal (SIGCHLD, SIG_DFL);
 }
 
+static void execute_command(const char *cmd) {
+    auto *windows_cmd = reinterpret_cast<wchar_t *>(g_utf8_to_utf16(cmd, -1, nullptr, nullptr, nullptr));
+    g_return_if_fail (windows_cmd);
 
-//static void bury_child (int signal){
-//    // noop for now
-//}
-
-static void execute_command (const char * cmd){
-    const std::wstring fileToRun = utf8_to_utf16(cmd);
     STARTUPINFOW si;
     _PROCESS_INFORMATION pi;
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
 
-    unsigned long flags = CREATE_UNICODE_ENVIRONMENT |  CREATE_NO_WINDOW;
-    auto * windows_cmd = const_cast<wchar_t *>(fileToRun.c_str());
+    unsigned long flags = CREATE_UNICODE_ENVIRONMENT | CREATE_NO_WINDOW;
     BOOL executed = ::CreateProcessW(NULL, windows_cmd, NULL, NULL, TRUE, flags, NULL, NULL, &si, &pi);
 
     if (!executed) {
-        AUDINFO((std::string("SongChange cannot run the command: ") + cmd + "\n").c_str());
+        AUDINFO("SongChange cannot run the command: %s\n", cmd);
     }
+    g_free(windows_cmd);
 }
-
 
 
 #else
