audacious-media-player/audacious-plugins

commit 592e3d6a653de2a06a91f341557ef9db098eceed

qtui: Enable song info popup on mouseover.

src/qtui/playlist-qt.cc
@@ -24,9 +24,8 @@
 
 #include <libaudcore/audstrings.h>
 #include <libaudcore/drct.h>
-#include <libaudcore/hook.h>
-#include <libaudcore/playlist.h>
 #include <libaudcore/runtime.h>
+#include <libaudqt/libaudqt.h>
 
 #include "playlist-qt.h"
 #include "playlist_header.h"
@@ -57,6 +56,7 @@ PlaylistWidget::PlaylistWidget (QWidget * parent, Playlist playlist) :
     setFrameShape (QFrame::NoFrame);
     setSelectionMode (ExtendedSelection);
     setDragDropMode (DragDrop);
+    setMouseTracking (true);
 
     updateSettings ();
     header->updateColumns ();
@@ -147,6 +147,25 @@ void PlaylistWidget::mouseDoubleClickEvent (QMouseEvent * event)
 
     if (event->button () == Qt::LeftButton)
         playCurrentIndex ();
+}
+
+void PlaylistWidget::mouseMoveEvent (QMouseEvent * event)
+{
+    int row = indexToRow (indexAt (event->pos ()));
+
+    if (row < 0)
+    {
+        hidePopup ();
+        return;
+    }
+
+    if (aud_get_bool (nullptr, "show_filepopup_for_tuple") && m_popup_pos != row)
+        triggerPopup (row);
+}
+
+void PlaylistWidget::leaveEvent (QEvent *)
+{
+    hidePopup ();
 }
 
 /* Since Qt doesn't support both DragDrop and InternalMove at once,
@@ -396,6 +415,28 @@ void PlaylistWidget::moveFocus (int distance)
     int row = currentIndex ().row ();
     row = aud::clamp (row + distance, 0, visibleRows - 1);
     setCurrentIndex (proxyModel->index (row, 0));
+}
+
+void PlaylistWidget::showPopup ()
+{
+    audqt::infopopup_show (m_playlist, m_popup_pos);
+}
+
+void PlaylistWidget::triggerPopup (int pos)
+{
+    audqt::infopopup_hide ();
+
+    m_popup_pos = pos;
+    m_popup_timer.queue (aud_get_int (nullptr, "filepopup_delay") * 100,
+     aud::obj_member<PlaylistWidget, & PlaylistWidget::showPopup>, this);
+}
+
+void PlaylistWidget::hidePopup ()
+{
+    audqt::infopopup_hide ();
+
+    m_popup_pos = -1;
+    m_popup_timer.stop ();
 }
 
 void PlaylistWidget::updateSettings ()

src/qtui/playlist-qt.h
@@ -23,6 +23,7 @@
 #include <QTreeView>
 
 #include <libaudcore/hook.h>
+#include <libaudcore/mainloop.h>
 #include <libaudcore/playlist.h>
 
 class PlaylistModel;
@@ -60,6 +61,9 @@ private:
     bool inUpdate = false;
     int firstVisibleColumn = 0;
 
+    int m_popup_pos = -1;
+    QueuedFunc m_popup_timer;
+
     QModelIndex rowToIndex (int row);
     int indexToRow (const QModelIndex & index);
 
@@ -70,10 +74,16 @@ private:
     void contextMenuEvent (QContextMenuEvent * event);
     void keyPressEvent (QKeyEvent * event);
     void mouseDoubleClickEvent (QMouseEvent * event);
+    void mouseMoveEvent (QMouseEvent * event);
+    void leaveEvent (QEvent *);
     void dragMoveEvent (QDragMoveEvent * event);
     void dropEvent (QDropEvent * event);
     void currentChanged (const QModelIndex & current, const QModelIndex & previous);
     void selectionChanged (const QItemSelection & selected, const QItemSelection & deselected);
+
+    void showPopup ();
+    void triggerPopup (int pos);
+    void hidePopup ();
 
     void updateSettings ();
 

commit e7a6c81f4b49872fb078368a113854b3461f37b5

qtui: Fix playlist drag-and-drop.  Closes: #831.
Regression caused by 592e3d6a6 (qtui: Enable song info popup on mouseover).

src/qtui/playlist-qt.cc
@@ -174,18 +174,18 @@ void PlaylistWidget::mouseMoveEvent (QMouseEvent * event)
     int row = indexToRow (indexAt (event->pos ()));
 
     if (row < 0)
-    {
         hidePopup ();
-        return;
-    }
-
-    if (aud_get_bool (nullptr, "show_filepopup_for_tuple") && m_popup_pos != row)
+    else if (aud_get_bool (nullptr, "show_filepopup_for_tuple") && m_popup_pos != row)
         triggerPopup (row);
+
+    audqt::TreeView::mouseMoveEvent (event);
 }
 
-void PlaylistWidget::leaveEvent (QEvent *)
+void PlaylistWidget::leaveEvent (QEvent * event)
 {
     hidePopup ();
+
+    audqt::TreeView::leaveEvent (event);
 }
 
 /* Since Qt doesn't support both DragDrop and InternalMove at once,

src/qtui/playlist-qt.h
@@ -74,7 +74,7 @@ private:
     void contextMenuEvent (QContextMenuEvent * event);
     void keyPressEvent (QKeyEvent * event);
     void mouseMoveEvent (QMouseEvent * event);
-    void leaveEvent (QEvent *);
+    void leaveEvent (QEvent * event);
     void dragMoveEvent (QDragMoveEvent * event);
     void dropEvent (QDropEvent * event);
     void currentChanged (const QModelIndex & current, const QModelIndex & previous);