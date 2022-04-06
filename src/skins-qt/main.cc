audacious-media-player/audacious-plugins

commit 94f9ed00dceaf59a984c29c1376566bb6cf14191

Use global volume and step size adjustments.

src/skins-qt/main.cc
@@ -76,6 +76,8 @@ public:
 
 private:
     DialogWindows m_dialogs;
+    int m_scroll_delta_x = 0;
+    int m_scroll_delta_y = 0;
 
     void draw (QPainter & cr);
     bool button_press (QMouseEvent * event);
@@ -462,18 +464,26 @@ static void stop_after_song_toggled ()
 
 bool MainWindow::scroll (QWheelEvent * event)
 {
-    int delta = event->angleDelta ().y () / 24;
-    if (delta)
-        mainwin_set_volume_diff (delta);
+    m_scroll_delta_x += event->angleDelta ().x ();
+    m_scroll_delta_y += event->angleDelta ().y ();
 
-#if 0
-        case GDK_SCROLL_LEFT:
-            aud_drct_seek (aud_drct_get_time () - 5000);
-            break;
-        case GDK_SCROLL_RIGHT:
-            aud_drct_seek (aud_drct_get_time () + 5000);
-            break;
-#endif
+    /* we want discrete steps here */
+    int steps_x = m_scroll_delta_x / 120;
+    int steps_y = m_scroll_delta_y / 120;
+
+    if (steps_x != 0)
+    {
+        m_scroll_delta_x -= 120 * steps_x;
+        int step_size = aud_get_int (0, "step_size");
+        aud_drct_seek (aud_drct_get_time () - steps_x * step_size * 1000);
+    }
+
+    if (steps_y != 0)
+    {
+        m_scroll_delta_y -= 120 * steps_y;
+        int volume_delta = aud_get_int (0, "volume_delta");
+        aud_drct_set_volume_main (aud_drct_get_volume_main () + steps_y * volume_delta);
+    }
 
     return true;
 }

src/skins/main.cc
@@ -479,16 +479,16 @@ bool MainWindow::scroll (GdkEventScroll * event)
     switch (event->direction)
     {
         case GDK_SCROLL_UP:
-            mainwin_set_volume_diff (5);
+            mainwin_set_volume_diff (aud_get_int (0, "volume_delta"));
             break;
         case GDK_SCROLL_DOWN:
-            mainwin_set_volume_diff (-5);
+            mainwin_set_volume_diff (-aud_get_int (0, "volume_delta"));
             break;
         case GDK_SCROLL_LEFT:
-            aud_drct_seek (aud_drct_get_time () - 5000);
+            aud_drct_seek (aud_drct_get_time () - aud_get_int (0, "step_size") * 1000);
             break;
         case GDK_SCROLL_RIGHT:
-            aud_drct_seek (aud_drct_get_time () + 5000);
+            aud_drct_seek (aud_drct_get_time () + aud_get_int (0, "step_size") * 1000);
             break;
         default:
             break;
@@ -533,12 +533,12 @@ bool Window::keypress (GdkEventKey * event)
         case GDK_KEY_Left:
         case GDK_KEY_KP_Left:
         case GDK_KEY_KP_7:
-            aud_drct_seek (aud_drct_get_time () - 5000);
+            aud_drct_seek (aud_drct_get_time () - aud_get_int (0, "step_size") * 1000);
             break;
         case GDK_KEY_Right:
         case GDK_KEY_KP_Right:
         case GDK_KEY_KP_9:
-            aud_drct_seek (aud_drct_get_time () + 5000);
+            aud_drct_seek (aud_drct_get_time () + aud_get_int (0, "step_size") * 1000);
             break;
         case GDK_KEY_KP_4:
             aud_drct_pl_prev ();

src/gtkui/gtkui.h
@@ -48,6 +48,5 @@ void popup_menu_rclick (unsigned button, uint32_t time);
 void popup_menu_tab (unsigned button, uint32_t time, Playlist playlist);
 void activate_search_tool ();
 void activate_playlist_manager ();
-void update_step_size ();
 
 #endif

src/gtkui/settings.cc
@@ -44,9 +44,6 @@ static const PreferencesWidget gtkui_widgets[] = {
     WidgetCheck (N_("Show column headers"),
         WidgetBool ("gtkui", "playlist_headers", redisplay_playlists)),
     WidgetLabel (N_("<b>Miscellaneous</b>")),
-    WidgetSpin (N_("Arrow keys seek by:"),
-        WidgetFloat ("gtkui", "step_size", update_step_size),
-        {0.1, 60, 0.1, N_("seconds")}),
     WidgetCheck (N_("Scroll on song change"),
         WidgetBool ("gtkui", "autoscroll"))
 };

src/gtkui/ui_gtk.cc
@@ -56,7 +56,6 @@ static const char * const gtkui_defaults[] = {
     "playlist_columns", "title artist album queued length",
     "playlist_headers", "TRUE",
     "show_remaining_time", "FALSE",
-    "step_size", "5",
 
     "player_x", "-1000",
     "player_y", "-1000",
@@ -416,10 +415,11 @@ static void set_slider_length (int length)
         gtk_widget_hide (slider);
 }
 
-void update_step_size ()
+static void update_step_size ()
 {
-    double step_size = aud_get_double ("gtkui", "step_size");
-    gtk_range_set_increments ((GtkRange *) slider, step_size * 1000, step_size * 1000);
+    int step_size = aud_get_int (0, "step_size");
+    // set half the step size because GTK doubles it for scroll events
+    gtk_range_set_increments ((GtkRange *) slider, step_size * 500, step_size * 500);
 }
 
 static void pause_cb ()
@@ -533,11 +533,11 @@ static gboolean window_keypress_cb (GtkWidget *, GdkEventKey * event)
             return true;
         case GDK_KEY_Left:
             if (aud_drct_get_playing ())
-                do_seek (aud_drct_get_time () - aud_get_double ("gtkui", "step_size") * 1000);
+                do_seek (aud_drct_get_time () - aud_get_int (0, "step_size") * 1000);
             return true;
         case GDK_KEY_Right:
             if (aud_drct_get_playing ())
-                do_seek (aud_drct_get_time () + aud_get_double ("gtkui", "step_size") * 1000);
+                do_seek (aud_drct_get_time () + aud_get_int (0, "step_size") * 1000);
             return true;
         }
 
@@ -572,11 +572,11 @@ static gboolean window_keypress_cb (GtkWidget *, GdkEventKey * event)
         {
           case GDK_KEY_Left:
             if (aud_drct_get_playing ())
-                do_seek (aud_drct_get_time () - aud_get_double ("gtkui", "step_size") * 1000);
+                do_seek (aud_drct_get_time () - aud_get_int (0, "step_size") * 1000);
             break;
           case GDK_KEY_Right:
             if (aud_drct_get_playing ())
-                do_seek (aud_drct_get_time () + aud_get_double ("gtkui", "step_size") * 1000);
+                do_seek (aud_drct_get_time () + aud_get_int (0, "step_size") * 1000);
             break;
           default:
             return false;
@@ -694,6 +694,7 @@ static void ui_hooks_associate ()
     hook_associate ("set record", update_toggles, nullptr);
     hook_associate ("set shuffle", update_toggles, nullptr);
     hook_associate ("set repeat", update_toggles, nullptr);
+    hook_associate ("set step_size", (HookFunction) update_step_size, nullptr);
     hook_associate ("config save", (HookFunction) config_save, nullptr);
 }
 
@@ -713,6 +714,7 @@ static void ui_hooks_disassociate ()
     hook_dissociate ("set record", update_toggles);
     hook_dissociate ("set shuffle", update_toggles);
     hook_dissociate ("set repeat", update_toggles);
+    hook_dissociate ("set step_size", (HookFunction) update_step_size);
     hook_dissociate ("config save", (HookFunction) config_save);
 }
 
@@ -859,8 +861,9 @@ bool GtkUI::init ()
     GtkIconSize icon_size = gtk_tool_shell_get_icon_size ((GtkToolShell *) toolbar);
     g_object_set ((GObject *) volume, "size", icon_size, nullptr);
     gtk_button_set_relief ((GtkButton *) volume, GTK_RELIEF_NONE);
+    int delta = aud_get_int (0, "volume_delta");
     gtk_scale_button_set_adjustment ((GtkScaleButton *) volume,
-     (GtkAdjustment *) gtk_adjustment_new (0, 0, 100, 1, 5, 0));
+     (GtkAdjustment *) gtk_adjustment_new (0, 0, 100, delta, delta, 0));
     gtk_widget_set_can_focus (volume, false);
 
     gtk_scale_button_set_value ((GtkScaleButton *) volume, aud_drct_get_volume_main ());

src/hotkey/gui.cc
@@ -73,8 +73,8 @@ static const char * event_desc[EVENT_MAX] = {
     [EVENT_PAUSE] = N_("Pause/Resume"),
     [EVENT_STOP] = N_("Stop"),
     [EVENT_NEXT_TRACK] = N_("Next track"),
-    [EVENT_FORWARD] = N_("Forward 5 seconds"),
-    [EVENT_BACKWARD] = N_("Rewind 5 seconds"),
+    [EVENT_FORWARD] = N_("Step forward"),
+    [EVENT_BACKWARD] = N_("Step backward"),
     [EVENT_MUTE] = N_("Mute"),
     [EVENT_VOL_UP] = N_("Volume up"),
     [EVENT_VOL_DOWN] = N_("Volume down"),

src/hotkey/plugin.cc
@@ -144,7 +144,7 @@ gboolean handle_keyevent (EVENT event)
         return true;
     }
 
-    /* decreace volume */
+    /* decrease volume */
     if (event == EVENT_VOL_DOWN)
     {
         if (mute)
@@ -154,7 +154,7 @@ gboolean handle_keyevent (EVENT event)
             mute = false;
         }
 
-        if ((current_volume -= plugin_cfg.vol_decrement) < 0)
+        if ((current_volume -= aud_get_int (0, "volume_delta")) < 0)
         {
             current_volume = 0;
         }
@@ -178,7 +178,7 @@ gboolean handle_keyevent (EVENT event)
             mute = false;
         }
 
-        if ((current_volume += plugin_cfg.vol_increment) > 100)
+        if ((current_volume += aud_get_int (0, "volume_delta")) > 100)
         {
             current_volume = 100;
         }
@@ -230,17 +230,14 @@ gboolean handle_keyevent (EVENT event)
     /* forward */
     if (event == EVENT_FORWARD)
     {
-        aud_drct_seek (aud_drct_get_time () + 5000);
+        aud_drct_seek (aud_drct_get_time () + aud_get_int (0, "step_size") * 1000);
         return true;
     }
 
     /* backward */
     if (event == EVENT_BACKWARD)
     {
-        int time = aud_drct_get_time ();
-        if (time > 5000) time -= 5000; /* Jump 5s back */
-            else time = 0;
-        aud_drct_seek (time);
+        aud_drct_seek (aud_drct_get_time () - aud_get_int (0, "step_size") * 1000);
         return true;
     }
 
@@ -341,10 +338,6 @@ void load_config ()
 {
     HotkeyConfiguration *hotkey;
     int i,max;
-
-    /* default volume level */
-    plugin_cfg.vol_increment = 4;
-    plugin_cfg.vol_decrement = 4;
 
     hotkey = &(plugin_cfg.first);
     hotkey->next = nullptr;

src/hotkey/plugin.h
@@ -40,9 +40,6 @@ typedef struct _HotkeyConfiguration {
 } HotkeyConfiguration;
 
 typedef struct {
-    int vol_increment;
-    int vol_decrement;
-
     /* keyboard */
     HotkeyConfiguration first;
 } PluginConfig;

commit ab71851bd19c19b8f7a8e78076c0b5c45df6255d

skins-qt: Prevent laggy volume slider when scrolling
This makes it also consistent with the GTK Winamp interface
which does not call aud_drct_set_volume_main() directly.

Reported at: https://redmine.audacious-media-player.org/boards/1/topics/2960
Regression from commit 94f9ed00dceaf59a984c.

src/skins-qt/main.cc
@@ -483,7 +483,7 @@ bool MainWindow::scroll (QWheelEvent * event)
     {
         m_scroll_delta_y -= 120 * steps_y;
         int volume_delta = aud_get_int ("volume_delta");
-        aud_drct_set_volume_main (aud_drct_get_volume_main () + steps_y * volume_delta);
+        mainwin_set_volume_diff (steps_y * volume_delta);
     }
 
     return true;