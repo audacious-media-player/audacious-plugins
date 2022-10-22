audacious-media-player/audacious-plugins

commit e6f39377ba1e634152b7d072a9984ce68d638730

search-tool-qt: Account for album artist. Closes: #1193

src/search-tool-qt/search-model.cc
@@ -148,6 +148,27 @@ void SearchModel::destroy_database ()
     m_database.clear ();
 }
 
+void SearchModel::add_to_database (int entry, std::initializer_list<Key> keys)
+{
+    Item * parent = nullptr;
+    auto hash = & m_database;
+
+    for (auto & key : keys)
+    {
+        if (! key.name)
+            continue;
+
+        Item * item = hash->lookup (key);
+        if (! item)
+            item = hash->add (key, Item (key.field, key.name, parent));
+
+        item->matches.append (entry);
+
+        parent = item;
+        hash = & item->children;
+    }
+}
+
 void SearchModel::create_database (Playlist playlist)
 {
     destroy_database ();
@@ -157,36 +178,33 @@ void SearchModel::create_database (Playlist playlist)
     for (int e = 0; e < entries; e ++)
     {
         Tuple tuple = playlist.entry_tuple (e, Playlist::NoWait);
+        String album_artist = tuple.get_str (Tuple::AlbumArtist);
+        String artist = tuple.get_str (Tuple::Artist);
 
-        aud::array<SearchField, String> fields;
-        fields[SearchField::Genre] = tuple.get_str (Tuple::Genre);
-        fields[SearchField::Artist] = tuple.get_str (Tuple::Artist);
-        fields[SearchField::Album] = tuple.get_str (Tuple::Album);
-        fields[SearchField::Title] = tuple.get_str (Tuple::Title);
-
-        Item * parent = nullptr;
-        SimpleHash<Key, Item> * hash = & m_database;
-
-        for (auto f : aud::range<SearchField> ())
+        if (album_artist && album_artist != artist)
         {
-            if (fields[f])
-            {
-                Key key = {f, fields[f]};
-                Item * item = hash->lookup (key);
-
-                if (! item)
-                    item = hash->add (key, Item (f, fields[f], parent));
-
-                item->matches.append (e);
-
-                /* genre is outside the normal hierarchy */
-                if (f != SearchField::Genre)
-                {
-                    parent = item;
-                    hash = & item->children;
-                }
-            }
+            /* album and song have different artists;
+             * add separately under respective artists */
+            add_to_database (e,
+             {{SearchField::Artist, album_artist},
+              {SearchField::Album, tuple.get_str (Tuple::Album)}});
+            add_to_database (e,
+             {{SearchField::Artist, artist},
+              {SearchField::Title, tuple.get_str (Tuple::Title)}});
         }
+        else
+        {
+            /* album and song have the same artist;
+             * add hierarchically under that artist */
+            add_to_database (e,
+             {{SearchField::Artist, artist},
+              {SearchField::Album, tuple.get_str (Tuple::Album)},
+              {SearchField::Title, tuple.get_str (Tuple::Title)}});
+        }
+
+        /* add separately under genre */
+        add_to_database (e,
+         {{SearchField::Genre, tuple.get_str (Tuple::Genre)}});
     }
 
     m_playlist = playlist;

src/search-tool-qt/search-model.h
@@ -100,6 +100,8 @@ protected:
     QMimeData * mimeData (const QModelIndexList & indexes) const;
 
 private:
+    void add_to_database (int entry, std::initializer_list<Key> keys);
+
     Playlist m_playlist;
     SimpleHash<Key, Item> m_database;
     Index<const Item *> m_items;

commit 974cdddd29635024691d953930e854c3058387a7

search-tool(-qt): Fix regression with multi-artist albums
Searching for the album name should also list tracks on the album,
and this was broken since e6f39377ba1e634152b7d072a9984ce68d638730.

src/search-tool-qt/search-model.cc
@@ -26,11 +26,6 @@
 
 static QString create_item_label (const Item & item)
 {
-    static constexpr aud::array<SearchField, const char *> start_tags =
-        {"", "<b>", "<i>", ""};
-    static constexpr aud::array<SearchField, const char *> end_tags =
-        {"", "</b>", "</i>", ""};
-
     QString string = start_tags[item.field];
 
     string += QString ((item.field == SearchField::Genre) ?
@@ -48,20 +43,19 @@ static QString create_item_label (const Item & item)
     {
         string += str_printf (dngettext (PACKAGE, "%d song", "%d songs",
          item.matches.len ()), item.matches.len ());
-
-        if (item.field == SearchField::Genre || item.parent)
-            string += ' ';
     }
 
     if (item.field == SearchField::Genre)
     {
+        string += ' ';
         string += _("of this genre");
     }
     else if (item.parent)
     {
         auto parent = (item.parent->parent ? item.parent->parent : item.parent);
 
-        string += (parent->field == SearchField::Album) ? _("on") : _("by");
+        string += ' ';
+        string += parent_prefix (parent->field);
         string += ' ';
         string += start_tags[parent->field];
         string += QString (parent->name).toHtmlEscaped ();
@@ -188,8 +182,12 @@ void SearchModel::create_database (Playlist playlist)
             add_to_database (e,
              {{SearchField::Artist, album_artist},
               {SearchField::Album, tuple.get_str (Tuple::Album)}});
+            /* add Title node under a HiddenAlbum node so that it can
+             * still be searched by album name (without listing the
+             * album twice) */
             add_to_database (e,
              {{SearchField::Artist, artist},
+              {SearchField::HiddenAlbum, tuple.get_str (Tuple::Album)},
               {SearchField::Title, tuple.get_str (Tuple::Title)}});
         }
         else
@@ -230,7 +228,8 @@ static void search_recurse (SimpleHash<Key, Item> & domain,
         }
 
         /* adding an item with exactly one child is redundant, so avoid it */
-        if (! new_mask && item.children.n_items () != 1)
+        if (! new_mask && item.children.n_items () != 1 &&
+         item.field != SearchField::HiddenAlbum)
             results.append (& item);
 
         search_recurse (item.children, terms, new_mask, results);

src/search-tool-qt/search-model.h
@@ -23,6 +23,7 @@
 #include <QAbstractListModel>
 
 #include <libaudcore/audstrings.h>
+#include <libaudcore/i18n.h>
 #include <libaudcore/multihash.h>
 #include <libaudcore/playlist.h>
 
@@ -30,9 +31,21 @@ enum class SearchField {
     Genre,
     Artist,
     Album,
+    HiddenAlbum,
     Title,
     count
 };
+
+static constexpr aud::array<SearchField, const char *> start_tags =
+    {"", "<b>", "<i>", "<i>", ""};
+static constexpr aud::array<SearchField, const char *> end_tags =
+    {"", "</b>", "</i>", "</i>", ""};
+
+static inline const char * parent_prefix (SearchField parent)
+{
+    return (parent == SearchField::Album || parent ==
+     SearchField::HiddenAlbum) ? _("on") : _("by");
+}
 
 struct Key
 {

src/search-tool/search-model.cc
@@ -68,8 +68,12 @@ void SearchModel::create_database (Playlist playlist)
             add_to_database (e,
              {{SearchField::Artist, album_artist},
               {SearchField::Album, tuple.get_str (Tuple::Album)}});
+            /* add Title node under a HiddenAlbum node so that it can
+             * still be searched by album name (without listing the
+             * album twice) */
             add_to_database (e,
              {{SearchField::Artist, artist},
+              {SearchField::HiddenAlbum, tuple.get_str (Tuple::Album)},
               {SearchField::Title, tuple.get_str (Tuple::Title)}});
         }
         else
@@ -110,7 +114,8 @@ static void search_recurse (SimpleHash<Key, Item> & domain,
         }
 
         /* adding an item with exactly one child is redundant, so avoid it */
-        if (! new_mask && item.children.n_items () != 1)
+        if (! new_mask && item.children.n_items () != 1 &&
+         item.field != SearchField::HiddenAlbum)
             results.append (& item);
 
         search_recurse (item.children, terms, new_mask, results);

src/search-tool/search-model.h
@@ -21,6 +21,7 @@
 #define SEARCHMODEL_H
 
 #include <libaudcore/audstrings.h>
+#include <libaudcore/i18n.h>
 #include <libaudcore/multihash.h>
 #include <libaudcore/playlist.h>
 
@@ -28,9 +29,21 @@ enum class SearchField {
     Genre,
     Artist,
     Album,
+    HiddenAlbum,
     Title,
     count
 };
+
+static constexpr aud::array<SearchField, const char *> start_tags =
+    {"", "<b>", "<i>", "<i>", ""};
+static constexpr aud::array<SearchField, const char *> end_tags =
+    {"", "</b>", "</i>", "</i>", ""};
+
+static inline const char * parent_prefix (SearchField parent)
+{
+    return (parent == SearchField::Album || parent ==
+     SearchField::HiddenAlbum) ? _("on") : _("by");
+}
 
 struct Key
 {

src/search-tool/search-tool.cc
@@ -268,11 +268,6 @@ static void action_add_to_playlist ()
 
 static void list_get_value (void * user, int row, int column, GValue * value)
 {
-    static constexpr aud::array<SearchField, const char *> start_tags =
-        {"", "<b>", "<i>", ""};
-    static constexpr aud::array<SearchField, const char *> end_tags =
-        {"", "</b>", "</i>", ""};
-
     auto escape = [] (const char * s)
         { return CharPtr (g_markup_escape_text (s, -1)); };
 
@@ -302,7 +297,7 @@ static void list_get_value (void * user, int row, int column, GValue * value)
         auto parent = (item.parent->parent ? item.parent->parent : item.parent);
 
         desc.insert (-1, " ");
-        desc.insert (-1, (parent->field == SearchField::Album) ? _("on") : _("by"));
+        desc.insert (-1, parent_prefix (parent->field));
         desc.insert (-1, " ");
         desc.insert (-1, start_tags[parent->field]);
         desc.insert (-1, escape (parent->name));