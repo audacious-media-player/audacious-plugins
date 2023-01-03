13fe29e8ec5e852fe3ea0b3b8976f4a9e3ece6e2
Thomas Lange <thomas-lange2@gmx.de>
2023-01-03 07:35:33 +0100

    CI: Implement build matrix for multiple OS's and build systems
    This allows us to build these configurations simultaneously
    and each one with Autotools and Meson:
    
    - Ubuntu 20.04 with GTK2 and Qt 5
    - Ubuntu 22.04 with GTK3 and Qt 5
    - macOS 12     with Qt 5 or  Qt 6

---
+++ .github/actions/install-dependencies/install-dependencies.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# --- Dependency configuration ---
+#
+# ubuntu-20.04:      Qt 5 + GTK2
+# ubuntu-22.04:      Qt 5 + GTK3
+# macOS (Autotools): Qt 5 - GTK
+# macOS (Meson):     Qt 6 - GTK
+
+os=$(tr '[:upper:]' '[:lower:]' <<< "$1")
+build_system=$(tr '[:upper:]' '[:lower:]' <<< "$2")
+
+if [ -z "$os" ] || [ -z "$build_system" ]; then
+  echo 'Invalid or missing input parameters'
+  exit 1
+fi
+
+ubuntu_packages='gettext libadplug-dev libasound2-dev libavformat-dev
+                 libbinio-dev libbs2b-dev libcddb2-dev libcdio-cdda-dev
+                 libcue-dev libcurl4-gnutls-dev libdbus-glib-1-dev
+                 libfaad-dev libflac-dev libfluidsynth-dev libgl1-mesa-dev
+                 libjack-jackd2-dev liblircclient-dev libmms-dev libmodplug-dev
+                 libmp3lame-dev libmpg123-dev libneon27-gnutls-dev libnotify-dev
+                 libopenmpt-dev libopusfile-dev libpulse-dev libqt5opengl5-dev
+                 libqt5x11extras5-dev libsamplerate0-dev libsdl1.2-dev
+                 libsidplayfp-dev libsndfile1-dev libsndio-dev libsoxr-dev
+                 libvorbis-dev libwavpack-dev libxml2-dev qtbase5-dev
+                 qtmultimedia5-dev'
+
+macos_packages='faad2 ffmpeg libbs2b libcue libmodplug libnotify libopenmpt
+                libsamplerate libsoxr neon sdl2 wavpack'
+
+case "$os" in
+  ubuntu-20.04)
+    if [ "$build_system" = 'meson' ]; then
+      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages libgtk2.0-dev meson
+    else
+      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages libgtk2.0-dev
+    fi
+    ;;
+
+  ubuntu*)
+    if [ "$build_system" = 'meson' ]; then
+      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages libgtk-3-dev libpipewire-0.3-dev meson
+    else
+      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages libgtk-3-dev libpipewire-0.3-dev
+    fi
+    ;;
+
+  macos*)
+    if [ "$build_system" = 'meson' ]; then
+      brew install $macos_packages qt@6 meson
+    else
+      brew install $macos_packages qt@5 automake
+    fi
+    ;;
+
+  *)
+    echo "Unsupported OS: $os"
+    exit 1
+    ;;
+esac
