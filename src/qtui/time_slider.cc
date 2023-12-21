eb326e16d12c8427f93265ce7783148ce50aefb5
Thomas Lange <thomas-lange2@gmx.de>
2023-12-21 14:57:53 +0100

    qtui: Add missing override keyword

--- src/qtui/time_slider.cc
+++ src/qtui/time_slider.cc
@@ -54,7 +54,7 @@ public:
 
     int styleHint(QStyle::StyleHint hint, const QStyleOption * option = nullptr,
                   const QWidget * widget = nullptr,
-                  QStyleHintReturn * returnData = nullptr) const
+                  QStyleHintReturn * returnData = nullptr) const override
     {
         int styleHint =
             QProxyStyle::styleHint(hint, option, widget, returnData);
