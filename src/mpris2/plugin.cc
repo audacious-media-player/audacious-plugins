0137b7f8e70ec2fa9798bfe76ee08bbc3b05439e
onegen <84882649+onegentig@users.noreply.github.com>
2024-07-31 22:59:37 +0200

    mpris2: Send more metadata about current track
    Add support for the following MPRIS fields:
    albumArtist, comment, genre, contentCreated, composer, asText, trackNumber, discNumber
    
    See also:
    - PR #165
    - https://www.freedesktop.org/wiki/Specifications/mpris-spec/metadata/

--- src/mpris2/plugin.cc
+++ src/mpris2/plugin.cc
@@ -20,9 +20,11 @@
 #include <math.h>
 #include <stdint.h>
 
+#include <libaudcore/audstrings.h>
 #include <libaudcore/drct.h>
 #include <libaudcore/hook.h>
 #include <libaudcore/i18n.h>
+#include <libaudcore/index.h>
 #include <libaudcore/interface.h>
 #include <libaudcore/plugin.h>
 #include <libaudcore/probe.h>
@@ -46,11 +48,7 @@ public:
 };
 
 EXPORT MPRIS2Plugin aud_plugin_instance;
-
 static GObject * object_core, * object_player;
-static String last_title, last_artist, last_album, last_file;
-static int last_length;
-static AudArtPtr image;
 
 static gboolean quit_cb (MprisMediaPlayer2 * object, GDBusMethodInvocation * call,
  void * unused)
@@ -68,96 +66,182 @@ static gboolean raise_cb (MprisMediaPlayer2 * object, GDBusMethodInvocation *
     return true;
 }
 
+/**
+ * @brief Struct for MPRIS2 metadata.
+ */
+struct MPRIS2Metadata
+{
+    String title;
+    String artist;
+    String album;
+    String album_artist;
+    String comment;
+    String genre;
+    String rec_date;
+    String composer;
+    String lyrics;
+    String file;
+    int32_t track = -1;
+    int64_t length = -1;
+    int32_t disc = -1;
+    AudArtPtr image;
+
+    bool operator== (const MPRIS2Metadata & other) const
+    {
+        return title == other.title && artist == other.artist
+               && album == other.album && album_artist == other.album_artist
+               && comment == other.comment && genre == other.genre
+               && composer == other.composer && file == other.file
+               && track == other.track && length == other.length
+               && disc == other.disc && rec_date == other.rec_date;
+    }
+
+    bool operator!= (const MPRIS2Metadata & other) const
+    {
+        return ! (*this == other);
+    }
+};
+
+static MPRIS2Metadata last_meta;
+
+/* Helper functions to handle GVariant creation */
+
+void add_g_variant_str (const char * key_str, const char * value_str,
+                       Index<GVariant *> & elems)
+{
+    if (! value_str || strlen (value_str) == 0)
+        return;
+
+    GVariant * key = g_variant_new_string (key_str);
+    GVariant * str = g_variant_new_string (value_str);
+    GVariant * var = g_variant_new_variant (str);
+    elems.append (g_variant_new_dict_entry (key, var));
+}
+
+void add_g_variant_int32 (const char * key_str, int32_t value_int,
+                         Index<GVariant *> & elems)
+{
+    GVariant * key = g_variant_new_string (key_str);
+    GVariant * num = g_variant_new_int32 (value_int);
+    GVariant * var = g_variant_new_variant (num);
+    elems.append (g_variant_new_dict_entry (key, var));
+}
+
+void add_g_variant_int64 (const char * key_str, int64_t value_int,
+                         Index<GVariant *> & elems)
+{
+    GVariant * key = g_variant_new_string (key_str);
+    GVariant * num = g_variant_new_int64 (value_int);
+    GVariant * var = g_variant_new_variant (num);
+    elems.append (g_variant_new_dict_entry (key, var));
+}
+
+void add_g_variant_arr_str (const char * key_str,
+                           ArrayRef<String> value_arr,
+                           Index<GVariant *> & elems)
+{
+    if (value_arr.len == 0)
+        return;
+
+    GVariant * key = g_variant_new_string (key_str);
+    Index<GVariant *> g_variant_arr;
+    for (const auto & item : value_arr)
+    {
+        if (! item || strlen (item) == 0)
+            continue;
+        g_variant_arr.append (g_variant_new_string (item));
+    }
+    GVariant * array = g_variant_new_array (
+        G_VARIANT_TYPE_STRING, g_variant_arr.begin (), g_variant_arr.len ());
+    GVariant * var = g_variant_new_variant (array);
+    elems.append (g_variant_new_dict_entry (key, var));
+}
+
 static void update_metadata (void * data, GObject * object)
 {
-    String title, artist, album, file;
-    int length = 0;
+    MPRIS2Metadata meta;
 
     if (aud_drct_get_ready ())
     {
         Tuple tuple = aud_drct_get_tuple ();
 
-        title = tuple.get_str (Tuple::Title);
-        artist = tuple.get_str (Tuple::Artist);
-        album = tuple.get_str (Tuple::Album);
-        length = tuple.get_int (Tuple::Length);
+        meta.title = tuple.get_str (Tuple::Title);
+        meta.artist = tuple.get_str (Tuple::Artist);
+        meta.album = tuple.get_str (Tuple::Album);
+        meta.album_artist = tuple.get_str (Tuple::AlbumArtist);
+        meta.comment = tuple.get_str (Tuple::Comment);
+        meta.genre = tuple.get_str (Tuple::Genre);
+        meta.composer = tuple.get_str (Tuple::Composer);
+        meta.lyrics = tuple.get_str (Tuple::Lyrics);
+        meta.track = tuple.get_int (Tuple::Track);
+        meta.length = tuple.get_int (Tuple::Length);
+        meta.disc = tuple.get_int (Tuple::Disc);
+        meta.file = aud_drct_get_filename ();
 
-        file = aud_drct_get_filename ();
+        int rec_year = tuple.get_int (Tuple::Year);
+        if (rec_year && rec_year > 0 && rec_year < 10000)
+        {
+            StringBuf rec_year_str = int_to_str (rec_year);
+            if (rec_year < 1000)
+            {
+                int zero_pad = 4 - rec_year_str.len ();
+                for (int i = 0; i < zero_pad; i++)
+                    rec_year_str.insert (0, "0", 1);
+            }
+            meta.rec_date = String (rec_year_str);
+        }
     }
 
-    if (title == last_title && artist == last_artist && album == last_album
-     && file == last_file && length == last_length)
+    if (meta == last_meta)
         return;
 
-    if (file != last_file)
-        image = file ? aud_art_request (file, AUD_ART_FILE) : AudArtPtr ();
+    if (meta.file != last_meta.file)
+        meta.image =
+            meta.file ? aud_art_request (meta.file, AUD_ART_FILE) : AudArtPtr ();
 
-    last_title = title;
-    last_artist = artist;
-    last_album = album;
-    last_file = file;
-    last_length = length;
+    Index<GVariant *> elems;
 
-    GVariant * elems[7];
-    int nelems = 0;
+    if (meta.title)
+        add_g_variant_str ("xesam:title", meta.title, elems);
+    if (meta.artist)
+        add_g_variant_arr_str ("xesam:artist", {{meta.artist}}, elems);
+    if (meta.album)
+        add_g_variant_str ("xesam:album", meta.album, elems);
+    if (meta.album_artist)
+        add_g_variant_arr_str ("xesam:albumArtist", {{meta.album_artist}}, elems);
+    if (meta.comment)
+        add_g_variant_arr_str ("xesam:comment", {{meta.comment}}, elems);
+    if (meta.genre)
+        add_g_variant_arr_str ("xesam:genre", {{meta.genre}}, elems);
+    if (meta.rec_date)
+        add_g_variant_str ("xesam:contentCreated", meta.rec_date, elems);
+    if (meta.composer)
+        add_g_variant_arr_str ("xesam:composer", {{meta.composer}}, elems);
+    if (meta.lyrics)
+        add_g_variant_str ("xesam:asText", meta.lyrics, elems);
+    if (meta.file)
+        add_g_variant_str ("xesam:url", meta.file, elems);
+    if (meta.track > 0)
+        add_g_variant_int32 ("xesam:trackNumber", meta.track, elems);
+    if (meta.length > 0)
+        add_g_variant_int64 ("mpris:length", meta.length * 1000, elems);
+    if (meta.disc > 0)
+        add_g_variant_int32 ("xesam:discNumber", meta.disc, elems);
 
-    if (title)
-    {
-        GVariant * key = g_variant_new_string ("xesam:title");
-        GVariant * str = g_variant_new_string (title);
-        GVariant * var = g_variant_new_variant (str);
-        elems[nelems ++] = g_variant_new_dict_entry (key, var);
-    }
-
-    if (artist)
-    {
-        GVariant * key = g_variant_new_string ("xesam:artist");
-        GVariant * str = g_variant_new_string (artist);
-        GVariant * array = g_variant_new_array (G_VARIANT_TYPE_STRING, & str, 1);
-        GVariant * var = g_variant_new_variant (array);
-        elems[nelems ++] = g_variant_new_dict_entry (key, var);
-    }
-
-    if (album)
-    {
-        GVariant * key = g_variant_new_string ("xesam:album");
-        GVariant * str = g_variant_new_string (album);
-        GVariant * var = g_variant_new_variant (str);
-        elems[nelems ++] = g_variant_new_dict_entry (key, var);
-    }
-
-    if (file)
-    {
-        GVariant * key = g_variant_new_string ("xesam:url");
-        GVariant * str = g_variant_new_string (file);
-        GVariant * var = g_variant_new_variant (str);
-        elems[nelems ++] = g_variant_new_dict_entry (key, var);
-    }
-
-    if (length > 0)
-    {
-        GVariant * key = g_variant_new_string ("mpris:length");
-        GVariant * val = g_variant_new_int64 ((int64_t) length * 1000);
-        GVariant * var = g_variant_new_variant (val);
-        elems[nelems ++] = g_variant_new_dict_entry (key, var);
-    }
-
-    auto image_file = image.file ();
-    if (image_file)
-    {
-        GVariant * key = g_variant_new_string ("mpris:artUrl");
-        GVariant * str = g_variant_new_string (image_file);
-        GVariant * var = g_variant_new_variant (str);
-        elems[nelems ++] = g_variant_new_dict_entry (key, var);
-    }
+    auto image_file = meta.image.file ();
+    add_g_variant_str ("mpris:artUrl", image_file, elems);
 
     GVariant * key = g_variant_new_string ("mpris:trackid");
-    GVariant * str = g_variant_new_object_path ("/org/mpris/MediaPlayer2/CurrentTrack");
+    GVariant * str =
+        g_variant_new_object_path ("/org/mpris/MediaPlayer2/CurrentTrack");
     GVariant * var = g_variant_new_variant (str);
-    elems[nelems ++] = g_variant_new_dict_entry (key, var);
+    elems.append (g_variant_new_dict_entry (key, var));
 
-    GVariant * array = g_variant_new_array (G_VARIANT_TYPE ("{sv}"), elems, nelems);
+    GVariant * array =
+        g_variant_new_array (G_VARIANT_TYPE ("{sv}"), elems.begin (), elems.len ());
     g_object_set (object, "metadata", array, nullptr);
+
+    last_meta = std::move (meta);
 }
 
 static void volume_changed (GObject * object)
@@ -289,13 +373,7 @@ void MPRIS2Plugin::cleanup ()
     g_object_unref (object_core);
     g_object_unref (object_player);
 
-    last_title = String ();
-    last_artist = String ();
-    last_album = String ();
-    last_file = String ();
-    last_length = 0;
-
-    image.clear ();
+    last_meta = MPRIS2Metadata();
 }
 
 bool MPRIS2Plugin::init ()
