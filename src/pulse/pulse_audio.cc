89251f97c08671ad34dba42c2eb69bfe1e188c37
Thomas Lange <thomas-lange2@gmx.de>
2025-02-01 22:20:28 +0100

    Revert "pulse: Use higher priority than PipeWire"
    Audacious automatically falls back to PulseAudio now
    with commit 434c518c513 if PipeWire is not available.
    
    This reverts commit 434c518c513b60738dea92dfee9db80c5dd8b6f1.

--- src/pulse/pulse_audio.cc
+++ src/pulse/pulse_audio.cc
@@ -45,19 +45,7 @@ public:
         & prefs
     };
 
-    /*
-     * Set a higher priority to that of PipeWire. Under a PulseAudio setup,
-     * the PipeWire output may be selected by default, which would promptly
-     * fail to work, as there is no PipeWire server/proxy running.
-     *
-     * On the other hand, under a PipeWire setup, PulseAudio output would
-     * still work normally, as pipewire-pulse acts as a PulseAudio server.
-     *
-     * Making PulseAudio the top preference allows for better compatibility
-     * for different setups, especially for distributions that have not (yet)
-     * switched to PipeWire as the default audio server.
-     */
-    constexpr PulseOutput () : OutputPlugin (info, 9) {}
+    constexpr PulseOutput () : OutputPlugin (info, 8) {}
 
     bool init ();
     void cleanup ();
