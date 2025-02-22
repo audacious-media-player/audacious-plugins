ab46802d35ccdbfcbd7695ec46b57b3a218e59a3
Thomas Lange <thomas-lange2@gmx.de>
2025-02-22 02:49:45 +0100

    CI: Don't build on Ubuntu 20.04 anymore
    GitHub has deprecated the runner image and will remove it in April.
    
    See also: https://github.com/actions/runner-images/issues/11101

--- .github/actions/install-dependencies/install-dependencies.sh
+++ .github/actions/install-dependencies/install-dependencies.sh
@@ -19,22 +19,23 @@ fi
 
 ubuntu_packages='gettext libadplug-dev libasound2-dev libavformat-dev
                  libbinio-dev libbs2b-dev libcddb2-dev libcdio-cdda-dev
-                 libcue-dev libcurl4-gnutls-dev
-                 libfaad-dev libflac-dev libfluidsynth-dev libgl1-mesa-dev
-                 libjack-jackd2-dev libjson-glib-dev libmms-dev libmodplug-dev
-                 libmp3lame-dev libmpg123-dev libneon27-gnutls-dev libnotify-dev
-                 libopenmpt-dev libopusfile-dev libpulse-dev libqt5opengl5-dev
+                 libcue-dev libcurl4-gnutls-dev libfaad-dev libflac-dev
+                 libfluidsynth-dev libgl1-mesa-dev libjack-jackd2-dev
+                 libjson-glib-dev libmms-dev libmodplug-dev libmp3lame-dev
+                 libmpg123-dev libneon27-gnutls-dev libnotify-dev libopenmpt-dev
+                 libopusfile-dev libpipewire-0.3-dev libpulse-dev
                  libsamplerate0-dev libsdl2-dev libsidplayfp-dev libsndfile1-dev
                  libsndio-dev libsoxr-dev libvorbis-dev libwavpack-dev libxml2-dev'
 
-ubuntu_qt5_packages='libqt5svg5-dev libqt5x11extras5-dev qtbase5-dev qtmultimedia5-dev'
+ubuntu_qt5_packages='libqt5opengl5-dev libqt5svg5-dev libqt5x11extras5-dev
+                     qtbase5-dev qtmultimedia5-dev'
 ubuntu_qt6_packages='qt6-base-dev qt6-multimedia-dev qt6-svg-dev'
 
 macos_packages='adplug faad2 ffmpeg libbs2b libcue libmms libmodplug libnotify
                 libopenmpt libsamplerate libsoxr neon opusfile sdl2 wavpack'
 
 case "$os" in
-  ubuntu-20.04)
+  ubuntu-22.04)
     if [ "$build_system" = 'meson' ]; then
       sudo apt-get -qq update && sudo apt-get install $ubuntu_packages $ubuntu_qt5_packages libgtk2.0-dev liblircclient-dev meson
     else
@@ -42,19 +43,11 @@ case "$os" in
     fi
     ;;
 
-  ubuntu-22.04)
-    if [ "$build_system" = 'meson' ]; then
-      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages $ubuntu_qt5_packages libgtk-3-dev liblircclient-dev libpipewire-0.3-dev meson
-    else
-      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages $ubuntu_qt5_packages libgtk-3-dev liblircclient-dev libpipewire-0.3-dev
-    fi
-    ;;
-
   ubuntu*)
     if [ "$build_system" = 'meson' ]; then
-      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages $ubuntu_qt6_packages libgtk-3-dev liblirc-dev libpipewire-0.3-dev meson
+      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages $ubuntu_qt6_packages libgtk-3-dev liblirc-dev meson
     else
-      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages $ubuntu_qt6_packages libgtk-3-dev liblirc-dev libpipewire-0.3-dev
+      sudo apt-get -qq update && sudo apt-get install $ubuntu_packages $ubuntu_qt6_packages libgtk-3-dev liblirc-dev
     fi
     ;;
 
