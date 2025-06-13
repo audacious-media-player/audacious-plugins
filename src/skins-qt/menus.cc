021156f57efee9ba32fc326387509f2c5048f9b2
SUN Haitao <sunhaitao@devtaste.com>
2025-06-13 12:45:00 +0800

    qtui: Add a toggle button for 'No Playlist Advance'

--- src/skins-qt/menus.cc
+++ src/skins-qt/menus.cc
@@ -95,7 +95,7 @@ static const audqt::MenuItem playback_items[] = {
     audqt::MenuToggle ({N_("Repeat"), "media-playlist-repeat", "R"}, {nullptr, "repeat", "set repeat"}),
     audqt::MenuToggle ({N_("Shuffle"), "media-playlist-shuffle", "S"}, {nullptr, "shuffle", "set shuffle"}),
     audqt::MenuToggle ({N_("Shuffle by Album")}, {nullptr, "album_shuffle", "set album_shuffle"}),
-    audqt::MenuToggle ({N_("No Playlist Advance"), nullptr, "Ctrl+N"}, {nullptr, "no_playlist_advance", "set no_playlist_advance"}),
+    audqt::MenuToggle ({N_("No Playlist Advance"), "no-playlist-advance", "Ctrl+N"}, {nullptr, "no_playlist_advance", "set no_playlist_advance"}),
     audqt::MenuToggle ({N_("Stop After This Song"), nullptr, "Ctrl+M"}, {nullptr, "stop_after_current_song", "set stop_after_current_song"}),
     audqt::MenuSep (),
     audqt::MenuCommand ({N_("Play"), "media-playback-start", "X"}, aud_drct_play),
