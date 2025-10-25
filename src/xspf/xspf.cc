84ef7f3e85e24d4dbe7532f894a1ad76c77fadae
Thomas Lange <thomas-lange2@gmx.de>
2025-10-25 20:37:38 +0200

    xspf: Unify coding style
    Also reduce nesting and use xmlStrEqual instead of xmlStrcmp.

--- src/xspf/xspf.cc
+++ src/xspf/xspf.cc
@@ -23,18 +23,14 @@
 #include <glib.h>
 #include <string.h>
 
-#include <libxml/tree.h>
 #include <libxml/parser.h>
-#include <libxml/xmlreader.h>
+#include <libxml/tree.h>
 #include <libxml/xmlsave.h>
-#include <libxml/xpath.h>
-#include <libxml/xpathInternals.h>
-#include <libxml/uri.h>
 
 #define AUD_GLIB_INTEGRATION
+#include <libaudcore/audstrings.h>
 #include <libaudcore/i18n.h>
 #include <libaudcore/plugin.h>
-#include <libaudcore/audstrings.h>
 
 #define XSPF_ROOT_NODE_NAME "playlist"
 #define XSPF_XMLNS "http://xspf.org/ns/0/"
@@ -42,10 +38,9 @@
 
 typedef struct {
     Tuple::Field tupleField;
-    const char *xspfName;
+    const char * xspfName;
     bool isMeta;
 } xspf_entry_t;
-
 
 static const xspf_entry_t xspf_entries[] = {
     {Tuple::Title, "title", false},
@@ -107,73 +102,77 @@ EXPORT XSPFLoader aud_plugin_instance;
 static void xspf_add_file (xmlNode * track, const char * filename,
  const char * base, Index<PlaylistAddItem> & items)
 {
-    xmlNode *nptr;
     String location;
     Tuple tuple;
 
-    for (nptr = track->children; nptr != nullptr; nptr = nptr->next) {
-        if (nptr->type == XML_ELEMENT_NODE) {
-            if (!xmlStrcmp(nptr->name, (xmlChar *)"location")) {
-                /* Location is a special case */
-                char *str = (char *)xmlNodeGetContent(nptr);
+    for (xmlNode * nptr = track->children; nptr; nptr = nptr->next)
+    {
+        if (nptr->type != XML_ELEMENT_NODE)
+            continue;
 
-                if (strstr (str, "://") != nullptr)
-                    location = String (str);
-                else if (str[0] == '/' && base != nullptr)
+        if (xmlStrEqual (nptr->name, (xmlChar *) "location"))
+        {
+            /* Location is a special case */
+            char * str = (char *) xmlNodeGetContent (nptr);
+
+            if (strstr (str, "://"))
+                location = String (str);
+            else if (str[0] == '/' && base)
+            {
+                const char * colon = strstr (base, "://");
+
+                if (colon)
+                    location = String (str_printf ("%.*s%s",
+                        (int) (colon + 3 - base), base, str));
+            }
+            else if (base)
+            {
+                const char * slash = strrchr (base, '/');
+
+                if (slash)
+                    location = String (str_printf ("%.*s%s",
+                        (int) (slash + 1 - base), base, str));
+            }
+
+            xmlFree (str);
+        }
+        else
+        {
+            /* Rest of the nodes are handled here */
+            bool isMeta = xmlStrEqual (nptr->name, (xmlChar *) "meta");
+            xmlChar * findName = isMeta
+                ? xmlGetProp (nptr, (xmlChar *) "rel")
+                : xmlStrdup (nptr->name);
+
+            for (const xspf_entry_t & entry : xspf_entries)
+            {
+                if (entry.isMeta != isMeta ||
+                 ! xmlStrEqual (findName, (xmlChar *) entry.xspfName))
+                    continue;
+
+                xmlChar * str = xmlNodeGetContent (nptr);
+
+                switch (Tuple::field_get_type (entry.tupleField))
                 {
-                    const char * colon = strstr (base, "://");
+                case Tuple::String:
+                    tuple.set_str (entry.tupleField, (char *) str);
+                    tuple.set_state (Tuple::Valid);
+                    break;
 
-                    if (colon != nullptr)
-                        location = String (str_printf ("%.*s%s",
-                         (int) (colon + 3 - base), base, str));
-                }
-                else if (base != nullptr)
-                {
-                    const char * slash = strrchr (base, '/');
+                case Tuple::Int:
+                    tuple.set_int (entry.tupleField, atol ((char *) str));
+                    tuple.set_state (Tuple::Valid);
+                    break;
 
-                    if (slash != nullptr)
-                        location = String (str_printf ("%.*s%s",
-                         (int) (slash + 1 - base), base, str));
-                }
-
-                xmlFree(str);
-            } else {
-                /* Rest of the nodes are handled here */
-                bool isMeta;
-                xmlChar *findName;
-
-                if (!xmlStrcmp(nptr->name, (xmlChar *)"meta")) {
-                    isMeta = true;
-                    findName = xmlGetProp(nptr, (xmlChar *)"rel");
-                } else {
-                    isMeta = false;
-                    findName = xmlStrdup(nptr->name);
-                }
-
-                for (const xspf_entry_t & entry : xspf_entries)
-                if ((entry.isMeta == isMeta) &&
-                    !xmlStrcmp(findName, (xmlChar *)entry.xspfName)) {
-                    xmlChar *str = xmlNodeGetContent(nptr);
-                    switch (Tuple::field_get_type (entry.tupleField)) {
-                        case Tuple::String:
-                            tuple.set_str (entry.tupleField, (char *)str);
-                            tuple.set_state (Tuple::Valid);
-                            break;
-
-                        case Tuple::Int:
-                            tuple.set_int (entry.tupleField, atol((char *)str));
-                            tuple.set_state (Tuple::Valid);
-                            break;
-
-                        default:
-                            break;
-                    }
-                    xmlFree(str);
+                default:
                     break;
                 }
 
-                xmlFree(findName);
+                xmlFree (str);
+                break;
             }
+
+            xmlFree (findName);
         }
     }
 
@@ -186,15 +185,15 @@ static void xspf_add_file (xmlNode * track, const char * filename,
     }
 }
 
-
 static void xspf_find_track (xmlNode * tracklist, const char * filename,
  const char * base, Index<PlaylistAddItem> & items)
 {
-    xmlNode *nptr;
+    for (xmlNode * nptr = tracklist->children; nptr; nptr = nptr->next)
+    {
+        if (nptr->type != XML_ELEMENT_NODE)
+            continue;
 
-    for (nptr = tracklist->children; nptr != nullptr; nptr = nptr->next) {
-        if (nptr->type == XML_ELEMENT_NODE &&
-         ! xmlStrcmp (nptr->name, (xmlChar *) "track"))
+        if (xmlStrEqual (nptr->name, (xmlChar *) "track"))
             xspf_add_file (nptr, filename, base, items);
     }
 }
@@ -221,40 +220,36 @@ bool XSPFLoader::load (const char * filename, VFSFile & file, String & title,
     if (! doc)
         return false;
 
-    xmlNode *nptr, *nptr2;
+    for (xmlNode * nptr = doc->children; nptr; nptr = nptr->next)
+    {
+        if (nptr->type != XML_ELEMENT_NODE ||
+         ! xmlStrEqual (nptr->name, (xmlChar *) "playlist"))
+            continue;
 
-    // find trackList
-    for (nptr = doc->children; nptr != nullptr; nptr = nptr->next) {
-        if (nptr->type == XML_ELEMENT_NODE &&
-            !xmlStrcmp(nptr->name, (xmlChar *)"playlist")) {
-            char * base;
+        char * base = (char *) xmlNodeGetBase (doc, nptr);
 
-            base = (char *)xmlNodeGetBase(doc, nptr);
+        for (xmlNode * nptr2 = nptr->children; nptr2; nptr2 = nptr2->next)
+        {
+            if (nptr2->type != XML_ELEMENT_NODE)
+                continue;
 
-            for (nptr2 = nptr->children; nptr2; nptr2 = nptr2->next)
+            if (xmlStrEqual (nptr2->name, (xmlChar *) "title"))
             {
-                if (nptr2->type != XML_ELEMENT_NODE)
-                    continue;
-
-                if (! xmlStrcmp (nptr2->name, (xmlChar *) "title"))
-                {
-                    xmlChar * xml_title = xmlNodeGetContent (nptr2);
-                    if (xml_title && xml_title[0])
-                        title = String ((char *) xml_title);
-                    xmlFree (xml_title);
-                }
-                else if (! xmlStrcmp (nptr2->name, (xmlChar *) "trackList"))
-                    xspf_find_track (nptr2, filename, base, items);
+                xmlChar * xml_title = xmlNodeGetContent (nptr2);
+                if (xml_title && xml_title[0])
+                    title = String ((char *) xml_title);
+                xmlFree (xml_title);
             }
-
-            xmlFree (base);
+            else if (xmlStrEqual (nptr2->name, (xmlChar *) "trackList"))
+                xspf_find_track (nptr2, filename, base, items);
         }
+
+        xmlFree (base);
     }
 
-    xmlFreeDoc(doc);
+    xmlFreeDoc (doc);
     return true;
 }
-
 
 #define IS_VALID_CHAR(c) (((c) >= 0x20 && (c) <= 0xd7ff) || \
                           ((c) == 0x9) || \
@@ -320,62 +315,58 @@ static bool is_valid_string (const char * s, CharPtr & subst)
     return false;
 }
 
-
-static void xspf_add_node (xmlNodePtr node, bool isMeta, const char * xspfName, const char * strVal)
+static void xspf_add_node (xmlNodePtr node, bool isMeta, const char * xspfName,
+ const char * strVal)
 {
     xmlNodePtr tmp;
     CharPtr subst;
 
-    if (isMeta) {
-        tmp = xmlNewNode(nullptr, (xmlChar *) "meta");
-        xmlSetProp(tmp, (xmlChar *) "rel", (xmlChar *) xspfName);
-    } else
-        tmp = xmlNewNode(nullptr, (xmlChar *) xspfName);
+    if (isMeta)
+    {
+        tmp = xmlNewNode (nullptr, (xmlChar *) "meta");
+        xmlSetProp (tmp, (xmlChar *) "rel", (xmlChar *) xspfName);
+    }
+    else
+        tmp = xmlNewNode (nullptr, (xmlChar *) xspfName);
 
     if (is_valid_string (strVal, subst))
         xmlAddChild (tmp, xmlNewText ((xmlChar *) strVal));
     else
         xmlAddChild (tmp, xmlNewText ((xmlChar *) subst.get ()));
 
-    xmlAddChild(node, tmp);
+    xmlAddChild (node, tmp);
 }
-
 
 bool XSPFLoader::save (const char * filename, VFSFile & file,
  const char * title, const Index<PlaylistAddItem> & items)
 {
-    xmlDocPtr doc;
-    xmlNodePtr rootnode, tracklist;
-
-    doc = xmlNewDoc((xmlChar *)"1.0");
+    xmlDocPtr doc = xmlNewDoc ((xmlChar *) "1.0");
     doc->charset = XML_CHAR_ENCODING_UTF8;
-    doc->encoding = xmlStrdup((xmlChar *)"UTF-8");
+    doc->encoding = xmlStrdup ((xmlChar *) "UTF-8");
 
-    rootnode = xmlNewNode(nullptr, (xmlChar *)XSPF_ROOT_NODE_NAME);
-    xmlSetProp(rootnode, (xmlChar *)"version", (xmlChar *)"1");
-    xmlSetProp(rootnode, (xmlChar *)"xmlns", (xmlChar *)XSPF_XMLNS);
+    xmlNodePtr rootnode = xmlNewNode (nullptr, (xmlChar *) XSPF_ROOT_NODE_NAME);
+    xmlSetProp (rootnode, (xmlChar *) "version", (xmlChar *) "1");
+    xmlSetProp (rootnode, (xmlChar *) "xmlns", (xmlChar *) XSPF_XMLNS);
 
-    /* common */
-    xmlDocSetRootElement(doc, rootnode);
+    xmlDocSetRootElement (doc, rootnode);
 
     if (title)
         xspf_add_node (rootnode, false, "title", title);
 
-    tracklist = xmlNewNode(nullptr, (xmlChar *)"trackList");
-    xmlAddChild(rootnode, tracklist);
+    xmlNodePtr tracklist = xmlNewNode (nullptr, (xmlChar *) "trackList");
+    xmlAddChild (rootnode, tracklist);
 
     for (auto & item : items)
     {
         const char * filename = item.filename;
         const Tuple & tuple = item.tuple;
-        xmlNodePtr track, location;
 
-        track = xmlNewNode(nullptr, (xmlChar *)"track");
-        location = xmlNewNode(nullptr, (xmlChar *)"location");
+        xmlNodePtr track = xmlNewNode (nullptr, (xmlChar *) "track");
+        xmlNodePtr location = xmlNewNode (nullptr, (xmlChar *) "location");
 
-        xmlAddChild(location, xmlNewText((xmlChar *)filename));
-        xmlAddChild(track, location);
-        xmlAddChild(tracklist, track);
+        xmlAddChild (location, xmlNewText ((xmlChar *) filename));
+        xmlAddChild (track, location);
+        xmlAddChild (tracklist, track);
 
         for (auto & entry : xspf_entries)
         {
@@ -385,10 +376,12 @@ bool XSPFLoader::save (const char * filename, VFSFile & file,
                 xspf_add_node (track, entry.isMeta, entry.xspfName,
                  tuple.get_str (entry.tupleField));
                 break;
+
             case Tuple::Int:
                 xspf_add_node (track, entry.isMeta, entry.xspfName,
                  int_to_str (tuple.get_int (entry.tupleField)));
                 break;
+
             default:
                 break;
             }
@@ -402,7 +395,7 @@ bool XSPFLoader::save (const char * filename, VFSFile & file,
     if (xmlSaveDoc (save, doc) < 0 || xmlSaveClose (save) < 0)
         goto ERR;
 
-    xmlFreeDoc(doc);
+    xmlFreeDoc (doc);
     return true;
 
 ERR:
